502ac041206164d28526fccabec63912cd63d279
Weiss Bai <whitesgr03@gmail.com>
2024-05-13 18:06:49 +0800

    Change PostDetail component
    Receiving a post prop to render the post information.

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,16 +1,28 @@
 import style from "../styles/PostDetail.module.css";
 
-const PostDetail = () => {
+import PropTypes from "prop-types";
+import { format } from "date-fns";
+
+const PostDetail = ({ post }) => {
 	return (
 		<ul className={style.postDetail}>
 			<li className={style.item}>
-				<strong className={style.dateTime}>Post Date Time</strong>
-				<h2>Post Title</h2>
-				<img src="#" alt="Post Title" />
-				<p>Post Content</p>
+				<strong className={style.dateTime}>
+					{format(post.createdAt, "MMMM d, y")}
+				</strong>
+				<h2>{post.title}</h2>
+				<img src={post.url} alt={post.title} />
+				<strong>
+					{`${format(post.createdAt, "MMMM d, y")} edited by author`}
+				</strong>
+				<p>{post.content}</p>
 			</li>
 		</ul>
 	);
+};
+
+PostDetail.propTypes = {
+	post: PropTypes.object,
 };
 
 export default PostDetail;
