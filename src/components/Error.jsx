6c423a960c23e44d67065461a78904161a454a1d
Weiss Bai <whitesgr03@gmail.com>
2024-05-14 17:12:31 +0800

    Add default param

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,12 @@ import style from "../styles/Error.module.css";
 
 import PropTypes from "prop-types";
 
-const Error = ({ error }) => (
+const Error = ({
+	error = {
+		title: "404 Not Found",
+		message: "The page you are looking for can't be found.",
+	},
+}) => (
 	<div className={style.error}>
 		<span className={style.alert}></span>
 		<h1 className={style.title}>{error.title}</h1>
