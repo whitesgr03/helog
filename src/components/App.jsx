742423a5f8764afc40bf0e1067656a0638342b6a
Weiss Bai <whitesgr03@gmail.com>
2024-05-14 11:06:54 +0800

    Change App component
    The App component will use the outlet component's context to pass
    some static data to the child components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
-
 import { Outlet } from "react-router-dom";
 
-function App() {
+const posts = [
+	{
+		id: "1",
+		url: "#",
+		title: "This is title A",
+		content: "This is content A",
+		createdAt: new Date("2024/5/1"),
+	},
+	{
+		id: "2",
+		url: "#",
+		title: "This is title B",
+		content: "This is content B",
+		createdAt: new Date("2024/5/1"),
+	},
+	{
+		id: "3",
+		url: "#",
+		title: "This is title C",
+		content: "This is content C",
+		createdAt: new Date("2024/5/1"),
+	},
+];
+
+const App = () => {
 	return (
 		<>
-			<Outlet />
+			<Outlet context={{ posts }} />
 		</>
 	);
-}
+};
 
 export default App;
