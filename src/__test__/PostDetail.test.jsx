2a8961f12df3d20acd82154980f5dbfc2e7b6dca
Weiss Bai <whitesgr03@gmail.com>
2024-05-14 21:23:13 +0800

    Change PostDetail component test

--- src/__test__/PostDetail.test.jsx
+++ src/__test__/PostDetail.test.jsx
@@ -1,71 +1,16 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { format } from "date-fns";
+// import { format } from "date-fns";
 // import { BrowserRouter } from "react-router-dom";
 
 import PostDetail from "../components/PostDetail";
 
 describe("PostDetail component", () => {
-	it("should render date time from props", () => {
-		const mockPost = {
-			id: "1",
-			url: "#",
-			title: "This is title",
-			content: "This is content",
-			createdAt: new Date("2024/5/1"),
-		};
+	it("should render correctly", () => {
+		const { asFragment } = render(<PostDetail />);
 
-		render(<PostDetail post={mockPost} />);
+		const actual = asFragment();
 
-		const actual = screen.queryByText(
-			format(mockPost.createdAt, "MMMM d, y")
-		);
-
-		expect(actual).not.equal(null);
-	});
-	it("should render title from props", () => {
-		const mockPost = {
-			id: "1",
-			url: "#",
-			title: "This is title",
-			content: "This is content",
-			createdAt: new Date("2024/5/1"),
-		};
-
-		render(<PostDetail post={mockPost} />);
-
-		const actual = screen.getByRole("heading", { name: mockPost.title });
-
-		expect(actual).not.equal(null);
-	});
-	it("should render image alt and url from props", () => {
-		const mockPost = {
-			id: "1",
-			url: "#",
-			title: "This is title",
-			content: "This is content",
-			createdAt: new Date("2024/5/1"),
-		};
-
-		render(<PostDetail post={mockPost} />);
-
-		const actual = screen.getByAltText(mockPost.title);
-
-		expect(actual).toHaveAttribute("src", mockPost.url);
-	});
-	it("should render content from props", () => {
-		const mockPost = {
-			id: "1",
-			url: "#",
-			title: "This is title",
-			content: "This is content",
-			createdAt: new Date("2024/5/1"),
-		};
-
-		render(<PostDetail post={mockPost} />);
-
-		const actual = screen.getByText(mockPost.content);
-
-		expect(actual).not.equal(null);
+		expect(actual).toMatchSnapshot();
 	});
 });
