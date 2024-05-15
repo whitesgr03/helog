037510de5ecbb8d4880d0899b6a2e9d37b129b84
Weiss Bai <whitesgr03@gmail.com>
2024-05-15 15:47:04 +0800

    Add Link component to wrap post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,21 +3,24 @@ import style from "../styles/PostList.module.css";
 import PropTypes from "prop-types";
 import { format } from "date-fns";
 
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, Link } from "react-router-dom";
 
 const PostList = () => {
 	const { posts } = useOutletContext();
 
 	const list = posts.map(post => (
 		<li key={post.id} className={style.item}>
-			<div className={style.imageWrap}>
-				<img src={post.url} alt={post.title} />
-			</div>
-			<strong className={style.dateTime}>
-				{format(post.createdAt, "MMMM d, y")}
-			</strong>
-			<h2>{post.title}</h2>
-			<p>{post.content}</p>
+			<Link to={`/posts/${post.id}`}>
+				<div className={style.imageWrap}>
+					<img src={post.url} alt={post.title} />
+				</div>
+				<strong className={style.dateTime}>
+					{format(post.createdAt, "MMMM d, y")}
+				</strong>
+				<h2>{post.title}</h2>
+
+				<p>{post.content}</p>
+			</Link>
 		</li>
 	));
 
