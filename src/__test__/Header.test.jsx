cb369e6494507aa7801af0297b3f6bb146700558
Weiss Bai <whitesgr03@gmail.com>
2024-05-15 20:03:21 +0800

    Add a test

--- src/__test__/Header.test.jsx
+++ src/__test__/Header.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
 import Header from "../components/Header";
@@ -13,5 +13,17 @@ describe("Header component", () => {
 		const actual = asFragment();
 
 		expect(actual).toMatchSnapshot();
+	});
+	it("should render the link, if the 'isAdmin' is true", () => {
+		const mockIsAdmin = true;
+		const linkText = "Write";
+
+		render(<Header isAdmin={mockIsAdmin} />, {
+			wrapper: BrowserRouter,
+		});
+
+		const actual = screen.queryByRole("link", { name: linkText });
+
+		expect(actual).not.toEqual(null);
 	});
 });
