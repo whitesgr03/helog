6bb48cb5847d9378c9390cf3fa6ccf6f97babe8a
Weiss Bai <whitesgr03@gmail.com>
2024-05-16 22:27:56 +0800

    The children replaced to Settings component

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -2,34 +2,39 @@ import style from "../styles/Settings.module.css";
 
 import PropTypes from "prop-types";
 
-const Settings = ({ userId, userName, userEmail }) => {
+const Settings = ({ user }) => {
+	// const user = useContext(userContext);
+	const activeChangeName = false;
+	const activeDeleteAccount = false;
 	return (
-		<div className={style.settings}>
-			<div className={style.header}>
-				<span>Settings</span>
-				<button className={`icon ${style.closeBtn}`}></button>
-			</div>
-			<div>
-				<div className={style.avatarWrap}>
-					<div className={style.avatar}>
-						{userName && userName.charAt(0).toUpperCase()}
-					</div>
+		<div className={model}>
+			<div className={style.settings}>
+				<button className={`icon ${style.closeBtn}`} />
+				<div className={style.header}>
+					<span>Settings</span>
 				</div>
-				<ul className={style.list}>
-					<li>
-						<strong className={style.title}>Name</strong>
+				<div>
+					<div className={style.avatarWrap}>
+						<div className={style.avatar}>
+							{user.name && user.name.charAt(0).toUpperCase()}
+						</div>
+					</div>
+					<ul className={style.list}>
+						<li>
+							<strong className={style.title}>Name</strong>
 
-						<span>{userName}</span>
-						<button>Change name</button>
-					</li>
-					<li>
-						<strong className={style.title}>Email</strong>
-						<span>{userEmail}</span>
-					</li>
-				</ul>
-				<div className={style.delete}>
-					<strong className={style.title}>Delete</strong>
-					<button>Delete account</button>
+							<span>{user.name}</span>
+							<button>Change name</button>
+						</li>
+						<li>
+							<strong className={style.title}>Email</strong>
+							<span>{user.email}</span>
+						</li>
+					</ul>
+					<div className={style.delete}>
+						<strong className={style.title}>Delete</strong>
+						<button>Delete account</button>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -37,9 +42,7 @@ const Settings = ({ userId, userName, userEmail }) => {
 };
 
 Settings.propTypes = {
-	userId: PropTypes.string,
-	userName: PropTypes.string,
-	userEmail: PropTypes.string,
+	user: PropTypes.object,
 };
 
 export default Settings;
