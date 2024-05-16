22d702f57609da638ca8bbfa8e14e56acd0b352c
Weiss Bai <whitesgr03@gmail.com>
2024-05-16 22:29:22 +0800

    Import module css file
    Import the form module css file to replace the form css file.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,19 @@
 import style from "../styles/Login.module.css";
-import "../styles/form.css";
+import form from "../styles/form.module.css";
 
 import { Link } from "react-router-dom";
 
 const Login = () => {
 	return (
 		<div className={style.login}>
-			<h3 className="title">Sign In</h3>
-			<div className="formWrap">
-				<form>
+			<h3 className={form.title}>Sign In</h3>
+			<div className={form.wrap}>
+				<form className={form.container}>
 					<label htmlFor="email">Email</label>
 					<input type="email" id="email" name="email" />
 					<label htmlFor="password">Password</label>
 					<input type="password" id="password" name="password" />
-					<button className="submitBtn" type="submit">
-						Login
-					</button>
+					<button type="submit">Login</button>
 				</form>
 				<div className={style.linkWrap}>
 					<p>New to HeLog?</p>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,12 @@
 import style from "../styles/Register.module.css";
-import "../styles/form.css";
+import form from "../styles/form.module.css";
 
 const Register = () => {
 	return (
 		<div className={style.register}>
-			<h3 className="title">Sign Up</h3>
-			<div className="formWrap">
-				<form>
+			<h3 className={form.title}>Sign Up</h3>
+			<div className={form.wrap}>
+				<form className={form.container}>
 					<label htmlFor="email">Email</label>
 					<input type="email" id="email" name="email" />
 					<label htmlFor="name">Name</label>
@@ -19,9 +19,7 @@ const Register = () => {
 						id="confirmPassword"
 						name="confirmPassword"
 					/>
-					<button className="submitBtn" type="submit">
-						Register
-					</button>
+					<button type="submit">Register</button>
 				</form>
 			</div>
 		</div>
