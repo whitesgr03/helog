5ad18b0fbb9fb0b4b45c5776d6f81007b6c2b4f4
Weiss Bai <whitesgr03@gmail.com>
2024-05-17 18:56:10 +0800

    Change Footer component
    Import module css files.
    
    Separate address element into component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,19 @@
 import style from "../styles/Footer.module.css";
+import { container } from "../styles/Contact.module.css";
+import { icon } from "../styles/image.module.css";
+import { Address } from "../components/Contact";
 
 const Footer = () => {
 	return (
 		<footer className={style.footer}>
-			<address className={style.contact}>
+			<div className={container}>
 				<a href="#git" alt="github">
-					<span className={`icon ${style.github}`} />
+					<span className={`${icon} ${style.github}`} />
 				</a>
-				<a className={style.link} href="mailto@whitesgr03@gmail.com">
-					<span className={`icon ${style.email}`} />
-					<em>whitesgr03@gmail.com</em>
-				</a>
-			</address>
+				<div className={style.address}>
+					<Address />
+				</div>
+			</div>
 			<p>&copy; 2024 Designed &amp; coded by Weiss Bai</p>
 		</footer>
 	);
