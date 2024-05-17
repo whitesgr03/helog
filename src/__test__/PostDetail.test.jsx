73111be987b2910a670267316746dc27af5e19b1
Weiss Bai <whitesgr03@gmail.com>
2024-05-17 14:32:00 +0800

    Add comment

--- src/__test__/PostDetail.test.jsx
+++ src/__test__/PostDetail.test.jsx
@@ -14,3 +14,97 @@ describe("PostDetail component", () => {
 		expect(actual).toMatchSnapshot();
 	});
 });
+
+// it("should render title, if post is true", () => {
+// 	const mockPosts = [
+// 		{
+// 			id: "1",
+// 			url: "#",
+// 			title: "This is title",
+// 			content: "This is content",
+// 			createdAt: new Date("2024/5/1"),
+// 			lastModified: new Date("2024/5/1"),
+// 		},
+// 	];
+// 	render(<PostDetail />);
+
+// 	const actual = screen.queryByRole("heading", {
+// 		level: 2,
+// 		name: mockPosts[0].title,
+// 	});
+
+// 	expect(actual).not.equal(null);
+// });
+// it("should render creation date, if post is true", () => {
+// 	const mockPosts = [
+// 		{
+// 			id: "1",
+// 			url: "#",
+// 			title: "This is title",
+// 			content: "This is content",
+// 			createdAt: new Date("2024/5/1"),
+// 			lastModified: new Date("2024/5/1"),
+// 		},
+// 	];
+// 	render(<PostDetail />);
+
+// 	const actual = screen.queryByText(
+// 		format(mockPosts[0].createdAt, "MMMM d, y")
+// 	);
+
+// 	expect(actual).not.equal(null);
+// });
+// it("should render last modified date, if post is true", () => {
+// 	const mockPosts = [
+// 		{
+// 			id: "1",
+// 			url: "#",
+// 			title: "This is title",
+// 			content: "This is content",
+// 			createdAt: new Date("2024/5/1"),
+// 			lastModified: new Date("2024/5/2"),
+// 		},
+// 	];
+// 	render(<PostDetail />);
+
+// 	const actual = screen.queryByText(
+// 		format(mockPosts[0].createdAt, "MMMM d, y")
+// 	);
+
+// 	expect(actual).not.equal(null);
+// });
+// it("should render image alt and url, if post is true", () => {
+// 	const mockPosts = [
+// 		{
+// 			id: "1",
+// 			url: "#",
+// 			title: "This is title",
+// 			content: "This is content",
+// 			createdAt: new Date("2024/5/1"),
+// 			lastModified: new Date("2024/5/1"),
+// 		},
+// 	];
+
+// 	render(<PostDetail />);
+
+// 	const actual = screen.getByAltText(mockPosts[0].title);
+
+// 	expect(actual).toHaveAttribute("src", mockPosts.url);
+// });
+// it("should render content, if post is true", () => {
+// 	const mockPosts = [
+// 		{
+// 			id: "1",
+// 			url: "#",
+// 			title: "This is title",
+// 			content: "This is content",
+// 			createdAt: new Date("2024/5/1"),
+// 			lastModified: new Date("2024/5/1"),
+// 		},
+// 	];
+// 	render(<PostDetail />);
+
+// 	const actual = screen.queryByText(mockPosts[0].content);
+
+// 	expect(actual).not.equal(null);
+// });
