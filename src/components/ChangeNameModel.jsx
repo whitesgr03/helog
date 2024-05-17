c4791fb5e5c94d6e785618c8be22db32775400fa
Weiss Bai <whitesgr03@gmail.com>
2024-05-17 14:30:59 +0800

    Change classes

--- src/components/ChangeNameModel.jsx
+++ src/components/ChangeNameModel.jsx
@@ -1,19 +1,24 @@
 import style from "../styles/ChangeNameModel.module.css";
+import { settings, closeBtn } from "../styles/Settings.module.css";
+import { form } from "../styles/form.module.css";
 import { blur } from "../styles/blur.module.css";
+import { successBtn } from "../styles/button.module.css";
 
 const ChangeNameModel = () => {
 	// const darkTheme = useContext(themContext)
 	// const user = useContext(userContext)
 	return (
 		<div className={blur}>
-			<form className={style.form}>
-				<button type="button" className={`icon ${style.closeBtn}`} />
-				<label htmlFor="changeName">Change Name</label>
-				<input id="changeName" type="text" name="name" />
-				<button className={style.submitBtn} type="submit">
-					Save
-				</button>
-			</form>
+			<div className={`${settings} ${style.model}`}>
+				<form className={form}>
+					<button type="button" className={`icon ${closeBtn}`} />
+					<label htmlFor="changeName">Change Name</label>
+					<input id="changeName" type="text" name="name" />
+					<button className={successBtn} type="submit">
+						Save
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 };
