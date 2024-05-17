4672751bc96c0c84e861ddae4d5f25ea82f7cc46
Weiss Bai <whitesgr03@gmail.com>
2024-05-17 14:28:36 +0800

    Change classes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,26 @@
 import style from "../styles/Login.module.css";
-import form from "../styles/form.module.css";
+import { form } from "../styles/form.module.css";
+import { successBtn } from "../styles/button.module.css";
 
 import { Link } from "react-router-dom";
 
 const Login = () => {
 	return (
 		<div className={style.login}>
-			<h3 className={form.title}>Sign In</h3>
-			<div className={form.wrap}>
-				<form className={form.container}>
+			<h3 className={style.title}>Sign In</h3>
+			<div className={style.formWrap}>
+				<form className={form}>
 					<label htmlFor="email">Email</label>
 					<input type="email" id="email" name="email" />
 					<label htmlFor="password">Password</label>
 					<input type="password" id="password" name="password" />
-					<button type="submit">Login</button>
+					<button type="submit" className={successBtn}>
+						Login
+					</button>
 				</form>
 				<div className={style.linkWrap}>
 					<p>New to HeLog?</p>
-					<Link to="/users/register" className={style.link}>
-						Create an account
-					</Link>
+					<Link to="/users/register">Create an account</Link>
 				</div>
 			</div>
 		</div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,27 +1,28 @@
 import style from "../styles/Register.module.css";
-import form from "../styles/form.module.css";
+import { form } from "../styles/form.module.css";
+import { successBtn } from "../styles/button.module.css";
 
 const Register = () => {
 	return (
 		<div className={style.register}>
-			<h3 className={form.title}>Sign Up</h3>
-			<div className={form.wrap}>
-				<form className={form.container}>
-					<label htmlFor="email">Email</label>
-					<input type="email" id="email" name="email" />
-					<label htmlFor="name">Name</label>
-					<input type="text" id="name" name="name" />
-					<label htmlFor="password">Password</label>
-					<input type="password" id="password" name="password" />
-					<label htmlFor="confirmPassword">Confirm Password</label>
-					<input
-						type="password"
-						id="confirmPassword"
-						name="confirmPassword"
-					/>
-					<button type="submit">Register</button>
-				</form>
-			</div>
+			<h3 className={style.title}>Sign Up</h3>
+			<form className={form}>
+				<label htmlFor="email">Email</label>
+				<input type="email" id="email" name="email" />
+				<label htmlFor="name">Name</label>
+				<input type="text" id="name" name="name" />
+				<label htmlFor="password">Password</label>
+				<input type="password" id="password" name="password" />
+				<label htmlFor="confirmPassword">Confirm Password</label>
+				<input
+					type="password"
+					id="confirmPassword"
+					name="confirmPassword"
+				/>
+				<button type="submit" className={successBtn}>
+					Register
+				</button>
+			</form>
 		</div>
 	);
 };
