706829a0a9990a177682aab2a67b305927b36f02
Weiss Bai <whitesgr03@gmail.com>
2024-05-20 00:26:55 +0800

    Create post component
    The "PostList" element splits the item element content into "Post"
    elements.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,30 +6,38 @@ import { format } from "date-fns";
 
 import { useOutletContext, Link } from "react-router-dom";
 
+const Post = ({ post }) => (
+	<Link to={`/posts/${post.id}`}>
+		<div className={imageWrap}>
+			<img src={post.url} alt={post.title} />
+		</div>
+		<strong className={style.dateTime}>
+			{format(post.createdAt, "MMMM d, y")}
+		</strong>
+		<h3>{post.title}</h3>
+
+		<p>{post.content}</p>
+	</Link>
+);
+
 const PostList = () => {
 	const { posts } = useOutletContext();
 
 	const list = posts.map(post => (
 		<li key={post.id}>
-			<Link to={`/posts/${post.id}`}>
-				<div className={imageWrap}>
-					<img src={post.url} alt={post.title} />
-				</div>
-				<strong className={style.dateTime}>
-					{format(post.createdAt, "MMMM d, y")}
-				</strong>
-				<h2>{post.title}</h2>
-
-				<p>{post.content}</p>
-			</Link>
+			<Post post={post} />
 		</li>
 	));
 
 	return <ul className={style.postList}>{list}</ul>;
 };
 
+Post.propTypes = {
+	post: PropTypes.object,
+};
+
 PostList.propTypes = {
 	posts: PropTypes.array,
 };
 
-export default PostList;
+export { PostList as default, Post };
