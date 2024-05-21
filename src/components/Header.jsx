0b2d35b259e0677360f7ee531e7a0692abed8408
Weiss Bai <whitesgr03@gmail.com>
2024-05-21 19:29:51 +0800

    Change Header component
    Add "darkTheme" variable to switch dark classes.
    
    Add "active" variable to handle model opening and closing.
    
    Add image css module classes.
    
    Add button css module classes.
    
    Add Dropdown css module sun and  moon class.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import style from "../styles/Header.module.css";
-import { icon } from "../styles/image.module.css";
-import { colorThemeToggle, sun, moon } from "../styles/Dropdown.module.css";
+import { sun, moon } from "../styles/Dropdown.module.css";
+
+import button from "../styles/utils/button.module.css";
+import image from "../styles/utils/image.module.css";
 
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -8,12 +10,11 @@ import PropTypes from "prop-types";
 import Dropdown from "./Dropdown";
 
 const Header = ({ user }) => {
-	// const darkTheme = useContext(themContext)
+	const darkTheme = true;
 	// const user = useContext(userContext);
-	const active = false;
+	const active = true;
 	return (
-		// <header className={`${darkTheme ? style.dark : ""} ${style.header} `}>
-		<header className={style.header}>
+		<header className={`${darkTheme ? style.dark : ""} ${style.header} `}>
 			<Link to="/" className={style.logo}>
 				<h1>HeLog</h1>
 			</Link>
@@ -22,29 +23,37 @@ const Header = ({ user }) => {
 					{user.isAdmin && (
 						<li>
 							<Link to="/">
-								<span className={`${icon} ${style.pencil}`} />
+								<span
+									className={`${image.icon} ${style.pencil}`}
+								/>
 								Write
 							</Link>
 						</li>
 					)}
 					<li>
-						<div className={style.switch}>
-							<div className={colorThemeToggle}>
-								{/* {darkTheme ? ( */}
-								{/* <span className={`${icon} ${moon}`} /> */}
-								{/* ) : ( */}
-								<span className={`${icon} ${sun}`} />
-								{/* )} */}
-								<button>
+						<button
+							className={`${darkTheme ? button.dark : ""} ${
+								button.theme
+							}`}
+						>
+							<div className={style.switch}>
+								<span
+									className={`${image.icon} ${
+										darkTheme ? moon : sun
+									}`}
+								/>
+								<div>
 									<div />
-								</button>
+								</div>
 							</div>
-							<span>Light mode</span>
-						</div>
+							<span>{darkTheme ? "Dark" : "Light"} mode</span>
+						</button>
 					</li>
 					<li>
 						<button>
-							<span className={`${icon} ${style.account}`} />
+							<span
+								className={`${image.icon} ${style.account}`}
+							/>
 							Account
 						</button>
 					</li>
