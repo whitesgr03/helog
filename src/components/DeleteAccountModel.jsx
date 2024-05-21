c454d2c3a2596ed2d7661e8a62eaabbd206cf7c3
Weiss Bai <whitesgr03@gmail.com>
2024-05-21 19:48:02 +0800

    Change DeleteAccountModel component
    Add "darkTheme" variable to switch dark classes.
    
    Add Settings css module dark and settings.
    
    Add blur css module classes.
    
    Add button css module classes.
    
    Add image css module classes.

--- src/components/DeleteAccountModel.jsx
+++ src/components/DeleteAccountModel.jsx
@@ -1,25 +1,47 @@
 import style from "../styles/DeleteAccountModel.module.css";
-import { blur } from "../styles/blur.module.css";
-import { button, errorBtn, closeBtn } from "../styles/button.module.css";
-import { settings } from "../styles/Settings.module.css";
-import { icon } from "../styles/image.module.css";
+import { dark as settingsDark, settings } from "../styles/Settings.module.css";
+
+import blur from "../styles/utils/blur.module.css";
+import button from "../styles/utils/button.module.css";
+import image from "../styles/utils/image.module.css";
 
 const DeleteAccountModel = () => {
-	// const darkTheme = useContext(themContext)
+	const darkTheme = true;
 	// const user = useContext(userContext)
 	return (
-		<div className={blur}>
-			<div className={`${settings} ${style.model}`}>
-				<button type="button" className={`${icon} ${closeBtn}`} />
-				<span>Delete Account</span>
+		<div className={blur.bgc}>
+			<div
+				className={`${
+					darkTheme ? `${style.dark} ${settingsDark}` : ""
+				} ${settings} ${style.model}`}
+			>
+				<button
+					type="button"
+					className={`${darkTheme ? button.dark : ""} ${
+						button.closeBtn
+					}`}
+				>
+					<span className={`${image.icon} ${button.close}`}></span>
+				</button>
+				<span className={style.title}>Delete Account</span>
 				<span className={style.content}>
 					Do you really want to delete?
 				</span>
 				<div className={style.buttonWrap}>
-					<button className={`${button} ${style.cancelBtn}`}>
+					<button
+						className={`${darkTheme ? button.dark : ""} ${
+							button.content
+						} ${style.cancelBtn}`}
+					>
 						Cancel
 					</button>
-					<button className={errorBtn}>Delete</button>
+					<button
+						className={`${darkTheme ? button.dark : ""} ${
+							button.error
+						}`}
+					>
+						Delete
+					</button>
 				</div>
 			</div>
 		</div>
