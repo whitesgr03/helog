6701322df5f886d0247db4abe98f555d61d33c20
Weiss Bai <whitesgr03@gmail.com>
2024-05-21 19:47:13 +0800

    Change Error component
    Add "darkTheme" variable to switch dark classes.
    
    Add image css module classes.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,5 @@
 import style from "../styles/Error.module.css";
-import { icon } from "../styles/image.module.css";
+import image from "../styles/utils/image.module.css";
 
 import PropTypes from "prop-types";
 
@@ -8,16 +8,19 @@ const Error = ({
 		title: "404 Not Found",
 		message: "The page you are looking for can't be found.",
 	},
-}) => (
-	<div className={style.error}>
-		<span className={`${icon} ${style.alert}`} />
-		<h1 className={style.title}>{error.title}</h1>
-		<div className={style.message}>
-			<p>Our apologies, there has been an error.</p>
-			<p>{error.message}</p>
+}) => {
+	const darkTheme = true;
+	return (
+		<div className={`${darkTheme ? style.dark : ""} ${style.error}`}>
+			<span className={`${image.icon} ${style.alert}`} />
+			<h1 className={style.title}>{error.title}</h1>
+			<div className={style.message}>
+				<p>Our apologies, there has been an error.</p>
+				<p>{error.message}</p>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 Error.propTypes = {
 	error: PropTypes.object,
