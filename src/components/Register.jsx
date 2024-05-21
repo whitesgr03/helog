4e37d44bbdc6eca0febea88bfce6e1a6b63784a4
Weiss Bai <whitesgr03@gmail.com>
2024-05-21 19:24:06 +0800

    Change REgister component
    Add darkTheme variable to switch dark classes.
    
    Add inputError variable to handle error message.
    
    Add form css module classes.
    
    Add button css module classes.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,25 +1,64 @@
 import style from "../styles/Register.module.css";
-import { form } from "../styles/form.module.css";
-import { successBtn } from "../styles/button.module.css";
+
+import form from "../styles/utils/form.module.css";
+import button from "../styles/utils/button.module.css";
 
 const Register = () => {
+	const darkTheme = true;
+	const inputError = true;
 	return (
-		<div className={style.register}>
+		<div className={`${darkTheme ? style.dark : ""} ${style.register}`}>
 			<h3 className={style.title}>Sign Up</h3>
-			<form className={form}>
-				<label htmlFor="email">Email</label>
-				<input type="email" id="email" name="email" />
-				<label htmlFor="name">Name</label>
-				<input type="text" id="name" name="name" />
-				<label htmlFor="password">Password</label>
-				<input type="password" id="password" name="password" />
-				<label htmlFor="confirmPassword">Confirm Password</label>
-				<input
-					type="password"
-					id="confirmPassword"
-					name="confirmPassword"
-				/>
-				<button type="submit" className={successBtn}>
+			<form className={`${darkTheme ? form.dark : ""} ${form.content}`}>
+				<div>
+					<label
+						htmlFor="registerEmail"
+						className={`${inputError ? form.error : ""}`}
+					>
+						Email
+						<input type="email" id="registerEmail" name="email" />
+					</label>
+					<span>This is a placeholder</span>
+				</div>
+				<div>
+					<label
+						htmlFor="registerName"
+						className={`${inputError ? form.error : ""}`}
+					>
+						Name
+						<input type="text" id="registerName" name="name" />
+					</label>
+					<span>This is a placeholder</span>
+				</div>
+				<div>
+					<label
+						htmlFor="registerPassword"
+						className={`${inputError ? form.error : ""}`}
+					>
+						Password
+						<input
+							type="password"
+							id="registerPassword"
+							name="password"
+						/>
+					</label>
+					<span>This is a placeholder</span>
+				</div>
+				<div>
+					<label
+						htmlFor="registerConfirmPassword"
+						className={`${inputError ? form.error : ""}`}
+					>
+						Confirm Password
+						<input
+							type="password"
+							id="registerConfirmPassword"
+							name="confirmPassword"
+						/>
+					</label>
+					<span>This is a placeholder</span>
+				</div>
+				<button type="submit" className={button.success}>
 					Register
 				</button>
 			</form>
