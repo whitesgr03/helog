4cf5a62b727cd8e010e7e2c1be8ad36af90e03df
Weiss Bai <whitesgr03@gmail.com>
2024-05-21 19:42:04 +0800

    Change Footer component
    Add "darkTheme" variable to switch dark classes.
    
    Add image css module classes.
    
    Add Contact css module dark and container.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,21 @@
 import style from "../styles/Footer.module.css";
-import { container } from "../styles/Contact.module.css";
-import { icon } from "../styles/image.module.css";
+import { dark as contactDark, container } from "../styles/Contact.module.css";
+
+import image from "../styles/utils/image.module.css";
+
 import { Address } from "../components/Contact";
 
 const Footer = () => {
+	const darkTheme = true;
 	return (
-		<footer className={style.footer}>
+		<footer
+			className={`${darkTheme ? `${style.dark} ${contactDark}` : ""}  ${
+				style.footer
+			}`}
+		>
 			<div className={container}>
-				<a href="#git" alt="github">
-					<span className={`${icon} ${style.github}`} />
+				<a href="#git" alt="github" className={style.link}>
+					<span className={`${image.icon} ${style.github}`} />
 				</a>
 				<div className={style.address}>
 					<Address />
