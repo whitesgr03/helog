d7a841b03f601a41d1dd14a06beee53f5f3ae57a
Weiss Bai <whitesgr03@gmail.com>
2024-05-23 23:22:26 +0800

    Change static data

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -9,11 +9,32 @@ import url from "../assets/bram-naus-n8Qb1ZAkK88-unsplash.jpg";
 
 const post = {
 	id: "1",
+	author: "DD",
 	url,
-	title: "This is title A",
-	content: "This is content A",
+	title: "Overview of the DevOps Interview Process: From Application to Selection-Part Terraform",
+	content: `AI language models, such as ChatGPT and Claude, empower anyone to create software. These models can intelligently understand problems, create solutions, and explain the solutions. But natural language isn’t always the best way to communicate with AI. If you need to keep track of complex data and define how you interact with that data in specific ways, SudoLang can help.
+
+If you think this is only going to help with AI code, think again. You can author any program in SudoLang and then transpile it to other languages, like JavaScript, Python, Rust, or C — so you can take advantage of these features no matter what kind of software you’re building.
+
+What is SudoLang?
+SudoLang is a programming language designed to collaborate with AI language models including ChatGPT, Bing Chat, Anthropic Claude, and Google Bard. It is designed to be easy to learn and use. It is also very expressive and powerful.
+
+All advanced language models understand it without any special prompting. You do not need to paste the SudoLang specification before using SudoLang with your favorite AI.
+
+Please read the SudoLang documentation for more information about the language.
+
+Interfaces in SudoLang
+Interface-oriented programming lets you structure your program and easily declare what you want the AI to keep track of and how you want to interact with it.
+
+Interfaces are a powerful tool to encapsulate related state, constraints, commands, and functions. They organize the code in a clear, understandable, and reusable way.
+
+The key features of interfaces in SudoLang include:
+
+State: This represents the data associated with the interface.
+Constraints: These are rules or requirements defined in natural language that the AI maintains automatically.
+/Commands and methods: These are operations that can be performed on or by the interface.`,
 	createdAt: new Date("2024/5/1"),
-	lastModified: new Date("2024/5/1"),
+	lastModified: new Date("2024/5/20"),
 };
 
 const PostDetail = () => {
