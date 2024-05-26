57d6c02b88884a8b585446c7c0e9bc03898e6899
Weiss Bai <whitesgr03@gmail.com>
2024-05-26 22:03:24 +0800

    Change Header component
    Add div with the transparent class.
    
    Add "toggleBtn" class.
    
    Add "handleActiveDropdown" and "handleCloseDropdown".

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { sun, moon } from "../styles/Dropdown.module.css";
 
 import button from "../styles/utils/button.module.css";
 import image from "../styles/utils/image.module.css";
+import { transparent } from "../styles/utils/blur.module.css";
 
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -13,55 +14,72 @@ import Dropdown from "./Dropdown";
 const Header = ({ user }) => {
 	const [activeDropdown, setActiveDropdown] = useState(false);
 	const darkTheme = false;
-	// const user = useContext(userContext);
+
+	const handleActiveDropdown = () => {
+		setActiveDropdown(!activeDropdown);
+	};
+	const handleCloseDropdown = () => {
+		setActiveDropdown(false);
+	};
 	return (
-		<header className={`${darkTheme ? style.dark : ""} ${style.header} `}>
-			<Link to="/" className={style.logo}>
-				<h1>HeLog</h1>
-			</Link>
-			<nav>
-				<ul className={style.list}>
-					{user.isAdmin && (
-						<li>
-							<Link to="/">
-								<span
-									className={`${image.icon} ${style.pencil}`}
-								/>
-								Write
-							</Link>
-						</li>
-					)}
-					<li>
-						<button
-							className={`${darkTheme ? button.dark : ""} ${
-								button.theme
-							}`}
-						>
-							<div className={style.switch}>
-								<span
-									className={`${image.icon} ${
-										darkTheme ? moon : sun
-									}`}
-								/>
-								<div>
-									<div />
+		<>
+			<header
+				className={`${darkTheme ? style.dark : ""} ${style.header} `}
+			>
+				<Link
+					to="/"
+					className={style.logo}
+					onClick={handleCloseDropdown}
+				>
+					<h1>HeLog</h1>
+				</Link>
+				<nav>
+					<ul className={style.list}>
+						{user.isAdmin && (
+							<li>
+								<Link to="/">
+									<span
+										className={`${image.icon} ${style.pencil}`}
+									/>
+									Write
+								</Link>
+							</li>
+						)}
+						<li className={style.toggleBtn}>
+							<button
+								className={`${darkTheme ? button.dark : ""} ${
+									button.theme
+								}`}
+							>
+								<div className={style.switch}>
+									<span
+										className={`${image.icon} ${
+											darkTheme ? moon : sun
+										}`}
+									/>
+									<div>
+										<div />
+									</div>
 								</div>
-							</div>
-							<span>{darkTheme ? "Dark" : "Light"} mode</span>
-						</button>
-					</li>
-					<li>
-						<button>
-							<span
-								className={`${image.icon} ${style.account}`}
-							/>
-							Account
-						</button>
-					</li>
-				</ul>
-			</nav>
-			{activeDropdown && <Dropdown user={user} />}
-		</header>
+								<span>{darkTheme ? "Dark" : "Light"} mode</span>
+							</button>
+						</li>
+						<li>
+							<button onClick={handleActiveDropdown}>
+								<span
+									className={`${image.icon} ${style.account}`}
+								/>
+								Account
+							</button>
+						</li>
+					</ul>
+				</nav>
+				{activeDropdown && <Dropdown user={user} />}
+			</header>
+			{activeDropdown && (
+				<div className={transparent} onClick={handleCloseDropdown} />
+			)}
+		</>
 	);
 };
 Header.propTypes = {
