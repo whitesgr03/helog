d312f1ea017e14377f2599423fcfa105f462a547
Weiss Bai <whitesgr03@gmail.com>
2024-05-28 16:17:29 +0800

    Refactor error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -6,27 +6,23 @@ import image from "../styles/utils/image.module.css";
 
 import { DarkThemeContext } from "../contexts/DarkThemeContext";
 
-const error = {
-	title: "404 Not Found",
-	message: "The page you are looking for can't be found.",
-};
-
-const Error = () => {
+const Error = ({ message }) => {
 	const [darkTheme] = useContext(DarkThemeContext);
 	return (
 		<div className={`${darkTheme ? style.dark : ""} ${style.error}`}>
-			<span className={`${image.icon} ${style.alert}`} />
-			<h1 className={style.title}>{error.title}</h1>
 			<div className={style.message}>
-				<p>Our apologies, there has been an error.</p>
-				<p>{error.message}</p>
+				<p>
+					<span className={`${image.icon} ${style.alert}`} />
+					Our apologies, there has been an error.
+				</p>
+				<p>{message}</p>
 			</div>
 		</div>
 	);
 };
 
 Error.propTypes = {
-	error: PropTypes.object,
+	message: PropTypes.string,
 };
 
 export default Error;
