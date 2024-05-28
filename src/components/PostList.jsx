5c5b6255a2d4faeae9b00489de703b49a5f32733
Weiss Bai <whitesgr03@gmail.com>
2024-05-28 16:21:28 +0800

    Add conditional to render Loading, Error or content.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,6 +7,10 @@ import style from "../styles/PostList.module.css";
 import image from "../styles/utils/image.module.css";
 
 import { DarkThemeContext } from "../contexts/DarkThemeContext";
+import Loading from "./Loading";
+import Error from "./Error";
+
+import url from "../assets/bram-naus-n8Qb1ZAkK88-unsplash.jpg";
 
 const Post = ({ post, darkTheme }) => (
 	<li className={style.item}>
@@ -30,14 +34,22 @@ const Post = ({ post, darkTheme }) => (
 const PostList = () => {
 	const [darkTheme] = useContext(DarkThemeContext);
 	const { posts } = useOutletContext();
+	const { data, error, loading } = posts;
 
-	const published = posts.filter(post => post.publish);
-	const items = published.map(post => <Post key={post.id} post={post} />);
+	const postsAddUrl = data.map(post => ({ ...post, url })); // <- Temporarily add url
+	const published = postsAddUrl.filter(post => post.publish);
+	const items = published.map(post => <Post key={post._id} post={post} />);
 
 	return (
-		<ul className={`${darkTheme ? style.dark : ""}  ${style.postList}`}>
-			{items}
-		</ul>
+		<div className={`${darkTheme ? style.dark : ""} ${style.postList}`}>
+			{loading ? (
+				<Loading />
+			) : error ? (
+				<Error message={"The posts could not be loaded."} />
+			) : (
+				<ul>{items}</ul>
+			)}
+		</div>
 	);
 };
 
