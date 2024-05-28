5b514fe6fb9aba54e1b1c4d3c90a62871a819d01
Weiss Bai <whitesgr03@gmail.com>
2024-05-29 00:11:10 +0800

    Refactor App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,14 @@
-import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 
-import style from "../styles/App.module.css";
-
-import Header from "./Header";
-import Footer from "./Footer";
-import Contact from "./Contact";
-
-import { DarkThemeContext } from "../contexts/DarkThemeContext";
 import useFetch from "../hooks/useFetch";
 
-const user = {
-	name: "Name",
-	isAdmin: true,
-	email: "Admin@gmail.com",
-};
 const getPostsUrl = "http://localhost:3000/blog/posts";
+// const getUserDetailUrl = "http://localhost:3000/blog/users/";
 
 const App = () => {
-	const [darkTheme] = useContext(DarkThemeContext);
 	const posts = useFetch(getPostsUrl);
-	return (
-		<div className={`${darkTheme ? style.dark : ""} ${style.app}`}>
-			<Header user={user} />
-			<div className={style.container}>
-				<main>
-					<Outlet context={{ posts, user }} />
-				</main>
-				<Contact />
-				<Footer />
-			</div>
-		</div>
-	);
+
+	return <Outlet context={{ posts }} />;
 };
 
 export default App;
