c3d8a6824e2e53555e4172b48052049accca294b
Weiss Bai <whitesgr03@gmail.com>
2024-05-29 18:21:50 +0800

    Remove comment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 
 const getPostsUrl = "http://localhost:3000/blog/posts";
-// const getUserDetailUrl = "http://localhost:3000/blog/users/";
 
 const App = () => {
 	const posts = useFetch(getPostsUrl);
