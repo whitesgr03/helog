1e572fb7ae60cf7b5bc2218447761e10b81824b0
Weiss Bai <whitesgr03@gmail.com>
2024-05-29 19:17:55 +0800

    Change Layout component
    Remove dark classes and DarkThemeContext.
    
    Add darkTheme, token and user states.
    
    Create "handleSwitchColorTheme" to get darkTheme from localStorage.
    
    Add useEffect to call "getToken" function getting token and checking
    token's exp.
    
    Add another useEffect to call "handleFetch" getting current user
    detail.
    
    Passing setToken, setUser, darkTheme, handleSwitchColorTheme to Header
    component.
    
    Passing setToken to Outlet component.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,33 +1,72 @@
-import { useState, useContext } from "react";
+import { useState, useEffect } from "react";
 import { Outlet, useOutletContext } from "react-router-dom";
 
-import style from "../styles/App.module.css";
+import style from "../styles/Layout.module.css";
 
 import Header from "./Header";
 import Footer from "./Footer";
 import Contact from "./Contact";
 
-import { DarkThemeContext } from "../contexts/DarkThemeContext";
+import handleFetch from "../utils/handleFetch";
+import handleColorScheme from "../utils/handleColorScheme";
 
-const user = {
-	name: "Name",
-	isAdmin: true,
-	email: "Admin@gmail.com",
-};
+const getUserDetailUrl = "http://localhost:3000/blog/user/";
 
 const Main = () => {
-	const [darkTheme] = useContext(DarkThemeContext);
 	const { posts } = useOutletContext();
-	const [token, setToken] = useState(
-		JSON.parse(localStorage.getItem("heLog")) ?? null
+	const [darkTheme, setDarkTheme] = useState(
+		JSON.parse(localStorage.getItem("darkTheme")) ?? handleColorScheme
 	);
+	const [token, setToken] = useState(null);
+	const [user, setUser] = useState(null);
+
+	const handleSwitchColorTheme = () => {
+		localStorage.setItem("darkTheme", JSON.stringify(!darkTheme));
+		setDarkTheme(!darkTheme);
+	};
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem("token"));
+
+		const getToken = () =>
+			Date.now() > data.exp
+				? localStorage.removeItem("token")
+				: setToken(data.token);
+
+		data && getToken();
+	}, []);
+
+	useEffect(() => {
+		const controller = new AbortController();
+		const { signal } = controller;
+		token &&
+			(async () => {
+				try {
+					const data = await handleFetch(getUserDetailUrl, {
+						signal,
+						headers: { Authorization: `Bearer ${token}` },
+					});
+
+					setUser(data);
+				} catch (err) {
+					!signal.aborted && console.log(err.cause);
+				}
+			})();
+		return () => controller.abort();
+	}, [token]);
 
 	return (
-		<div className={`${darkTheme ? style.dark : ""} ${style.app}`}>
-			<Header user={user} token={token} />
+		<div className={` ${darkTheme ? "dark" : ""} ${style.layout}`}>
+			<Header
+				user={user}
+				setToken={setToken}
+				setUser={setUser}
+				darkTheme={darkTheme}
+				handleSwitchColorTheme={handleSwitchColorTheme}
+			/>
 			<div className={style.container}>
 				<main>
-					<Outlet context={{ posts, user }} />
+					<Outlet context={{ posts, user, setToken }} />
 				</main>
 				<Contact />
 				<Footer />
