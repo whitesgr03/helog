2d787883cc4e74d6b5fe0dd88aa6ac608d31b6e7
Weiss Bai <whitesgr03@gmail.com>
2024-06-03 19:41:40 +0800

    Refactor Login and Register components
    Import yup to check input fields.
    
    Add "handleValidFields" to check input value that user entered.
    
    Add "handleChange" to change each input value.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,84 +1,150 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useOutletContext, Link, useNavigate } from "react-router-dom";
+import { object, string } from "yup";
 
 import style from "../styles/Login.module.css";
 import form from "../styles/utils/form.module.css";
 import button from "../styles/utils/button.module.css";
+import image from "../styles/utils/image.module.css";
 
 import handleFetch from "../utils/handleFetch";
 
 const POST_LOGIN_URL = "http://localhost:3000/blog/users/login";
 
+const defaultForm = { email: "", password: "" };
+
 const Login = () => {
+	const [formFields, setFormFields] = useState(defaultForm);
+	const [errors, setErrors] = useState(null);
+	const timer = useRef(null);
+
 	const { setToken } = useOutletContext();
-	const [errors, setErrors] = useState({});
 	const navigate = useNavigate();
 
-	const handelSubmit = async e => {
-		e.preventDefault();
-		const formData = new FormData(e.target);
-		const formProps = Object.fromEntries(formData);
+	const handleValidFields = async fields => {
+		let isValid = false;
 
-		const fetchOption = {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		};
+		const schema = object({
+			email: string()
+				.trim()
+				.lowercase()
+				.required("The email is required.")
+				.matches(
+					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+					"The email must be in the correct format."
+				),
+			password: string()
+				.trim()
+				.required("The password is required.")
+				.min(8, "The password is incorrect."),
+		}).noUnknown();
+
 		try {
-			const data = await handleFetch(POST_LOGIN_URL, {
-				...fetchOption,
-				body: JSON.stringify(formProps),
+			await schema.validate(fields, {
+				abortEarly: false,
 			});
 
-			localStorage.setItem("token", JSON.stringify(data));
-			setToken(data.token);
-			navigate("/", { replace: true });
+			setErrors({});
+			isValid = true;
+			return isValid;
 		} catch (err) {
 			const obj = {};
-			for (const error of err.cause) {
-				obj[error.field] = error.message;
+			for (const error of err.inner) {
+				obj[error.path] ?? (obj[error.path] = error.message);
 			}
 			setErrors(obj);
+			return isValid;
 		}
+	};
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		const isValid = await handleValidFields(formFields);
+
+		const handleLogin = async () => {
+			const fetchOption = {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formFields),
+			};
+			try {
+				const data = await handleFetch(POST_LOGIN_URL, fetchOption);
+				localStorage.setItem("token", JSON.stringify(data));
+				setToken(data.token);
+				navigate("/", { replace: true });
+			} catch (err) {
+				const obj = {};
+				for (const error of err.cause) {
+					obj[error.field] = error.message;
+				}
+				setErrors(obj);
+			}
+		};
+
+		isValid && (await handleLogin());
+	};
+
+	const handleChange = e => {
+		const { name, value } = e.target;
+		const fields = {
+			...formFields,
+			[name]: value,
+		};
+		setFormFields(fields);
+
+		errors && timer.current && clearTimeout(timer.current);
+		errors &&
+			(timer.current = setTimeout(() => handleValidFields(fields), 500));
 	};
 
 	return (
 		<div className={style.login}>
 			<h3 className={style.title}>Sign In</h3>
 			<div className={style.formWrap}>
-				<form className={form.content} onSubmit={handelSubmit}>
+				<form className={form.content} onSubmit={handleSubmit}>
 					<div>
 						<label
-							htmlFor="loginEmail"
-							className={`${errors.email ? form.error : ""}`}
+							htmlFor="email"
+							className={`${errors?.email ? form.error : ""}`}
 						>
 							Email
-							<input type="email" id="loginEmail" name="email" />
+							<input
+								id="email"
+								type="email"
+								name="email"
+								value={formFields.email}
+								onChange={handleChange}
+							/>
 						</label>
-						<span>
-							{errors.email
-								? errors.email
-								: "This is a placeholder"}
-						</span>
+						<div>
+							<span className={`${image.icon} ${form.alert}`} />
+							<span className={form.placeholder}>
+								{errors?.email ?? "This is a placeholder"}
+							</span>
+						</div>
 					</div>
 					<div>
 						<label
-							htmlFor="loginPassword"
-							className={`${errors.password ? form.error : ""}`}
+							htmlFor="password"
+							className={`${errors?.password ? form.error : ""}`}
 						>
 							Password
 							<input
+								id="password"
 								type="password"
-								id="loginPassword"
 								name="password"
+								value={formFields.password}
+								onChange={handleChange}
 							/>
 						</label>
-						<span>
-							{errors.password
-								? errors.password
-								: "This is a placeholder"}
-						</span>
+						<div>
+							<span className={`${image.icon} ${form.alert}`} />
+							<span className={form.placeholder}>
+								{errors?.password ?? "This is a placeholder"}
+							</span>
+						</div>
 					</div>
 					<button type="submit" className={button.success}>
 						Login
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,72 +1,209 @@
+import { useState, useRef } from "react";
+import { useOutletContext, useNavigate } from "react-router-dom";
+import { object, string, ref } from "yup";
+
 import style from "../styles/Register.module.css";
 import form from "../styles/utils/form.module.css";
 import button from "../styles/utils/button.module.css";
+import image from "../styles/utils/image.module.css";
+
+import handleFetch from "../utils/handleFetch";
+
+const POST_REGISTER_URL = "http://localhost:3000/blog/users";
+
+const defaultForm = { name: "", email: "", password: "", confirmPassword: "" };
 
 const Register = () => {
-	const inputError = true;
+	const [formFields, setFormFields] = useState(defaultForm);
+	const [errors, setErrors] = useState(null);
+	const timer = useRef(null);
 
-	const handelSubmit = e => {
-		e.preventDefault();
-		const isVerify = false;
-		const formData = new FormData(e.target);
-		const formProps = Object.fromEntries(formData);
+	const { setToken } = useOutletContext();
+	const navigate = useNavigate();
 
-		console.log(formProps);
+	const handleValidFields = async fields => {
+		let isValid = false;
 
-		isVerify && console.log("Post request", JSON.stringify(formProps));
-		isVerify && e.target.reset();
+		const schema = object({
+			name: string()
+				.trim()
+				.required("The name is required.")
+				.max(30, ({ max }) => `The name must be less than ${max} long.`)
+				.matches(
+					/^[a-zA-Z]\w*$/,
+					"The name must be alphanumeric and underscore."
+				),
+			email: string()
+				.trim()
+				.lowercase()
+				.required("The email is required.")
+				.matches(
+					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+					"The email must be in the correct format."
+				),
+			password: string()
+				.trim()
+				.required("The password is required.")
+				.min(8, "The password is incorrect."),
+			confirmPassword: string()
+				.trim()
+				.required("The confirm password is required.")
+				.oneOf(
+					[ref("password"), null],
+					"The confirmation password does not match the password."
+				),
+		}).noUnknown();
+
+		try {
+			await schema.validate(fields, {
+				abortEarly: false,
+			});
+
+			setErrors({});
+			isValid = true;
+			return isValid;
+		} catch (err) {
+			const obj = {};
+			for (const error of err.inner) {
+				obj[error.path] ?? (obj[error.path] = error.message);
+			}
+			setErrors(obj);
+			return isValid;
+		}
 	};
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		const isValid = await handleValidFields(formFields);
+
+		const handleLogin = async () => {
+			const fetchOption = {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formFields),
+			};
+			try {
+				const data = await handleFetch(POST_REGISTER_URL, fetchOption);
+				localStorage.setItem("token", JSON.stringify(data));
+				setToken(data.token);
+				navigate("/", { replace: true });
+			} catch (err) {
+				console.log(err);
+				const obj = {};
+				for (const error of err.cause) {
+					obj[error.field] = error.message;
+				}
+				setErrors(obj);
+			}
+		};
+
+		isValid && (await handleLogin());
+	};
+
+	const handleChange = e => {
+		const { name, value } = e.target;
+		const fields = {
+			...formFields,
+			[name]: value,
+		};
+		setFormFields(fields);
+
+		errors && timer.current && clearTimeout(timer.current);
+		errors &&
+			(timer.current = setTimeout(() => handleValidFields(fields), 500));
+	};
+
 	return (
 		<div className={style.register}>
 			<h3 className={style.title}>Sign Up</h3>
-			<form className={form.content} onSubmit={handelSubmit}>
+			<form className={form.content} onSubmit={handleSubmit}>
 				<div>
 					<label
-						htmlFor="registerEmail"
-						className={`${inputError ? form.error : ""}`}
-					>
-						Email
-						<input type="email" id="registerEmail" name="email" />
-					</label>
-					<span>This is a placeholder</span>
-				</div>
-				<div>
-					<label
-						htmlFor="registerName"
-						className={`${inputError ? form.error : ""}`}
+						htmlFor="name"
+						className={`${errors?.name ? form.error : ""}`}
 					>
 						Name
-						<input type="text" id="registerName" name="name" />
+						<input
+							id="name"
+							type="text"
+							name="name"
+							value={formFields.name}
+							onChange={handleChange}
+						/>
 					</label>
-					<span>This is a placeholder</span>
+					<div>
+						<span className={`${image.icon} ${form.alert}`} />
+						<span className={form.placeholder}>
+							{errors?.name ?? "This is a placeholder"}
+						</span>
+					</div>
 				</div>
 				<div>
 					<label
-						htmlFor="registerPassword"
-						className={`${inputError ? form.error : ""}`}
+						htmlFor="email"
+						className={`${errors?.email ? form.error : ""}`}
+					>
+						Email
+						<input
+							id="email"
+							type="email"
+							name="email"
+							value={formFields.email}
+							onChange={handleChange}
+						/>
+					</label>
+					<div>
+						<span className={`${image.icon} ${form.alert}`} />
+						<span className={form.placeholder}>
+							{errors?.email ?? "This is a placeholder"}
+						</span>
+					</div>
+				</div>
+				<div>
+					<label
+						htmlFor="password"
+						className={`${errors?.password ? form.error : ""}`}
 					>
 						Password
 						<input
+							id="password"
 							type="password"
-							id="registerPassword"
 							name="password"
+							value={formFields.password}
+							onChange={handleChange}
 						/>
 					</label>
-					<span>This is a placeholder</span>
+					<div>
+						<span className={`${image.icon} ${form.alert}`} />
+						<span className={form.placeholder}>
+							{errors?.password ?? "This is a placeholder"}
+						</span>
+					</div>
 				</div>
 				<div>
 					<label
-						htmlFor="registerConfirmPassword"
-						className={`${inputError ? form.error : ""}`}
+						htmlFor="confirmPassword"
+						className={`${
+							errors?.confirmPassword ? form.error : ""
+						}`}
 					>
 						Confirm Password
 						<input
+							id="confirmPassword"
 							type="password"
-							id="registerConfirmPassword"
 							name="confirmPassword"
+							value={formFields.confirmPassword}
+							onChange={handleChange}
 						/>
 					</label>
-					<span>This is a placeholder</span>
+					<div>
+						<span className={`${image.icon} ${form.alert}`} />
+						<span className={form.placeholder}>
+							{errors?.confirmPassword ?? "This is a placeholder"}
+						</span>
+					</div>
 				</div>
 				<button type="submit" className={button.success}>
 					Register
