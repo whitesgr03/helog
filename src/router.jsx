c6f9cfca04a15f570ebe64af36f69410da4a65da
Weiss Bai <whitesgr03@gmail.com>
2024-06-03 18:05:00 +0800

    Remove Layout route

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/index.css";
 
 import App from "./components/App";
-import Layout from "./components/Layout";
 import NotFound from "./components/NotFound";
 import Home from "./components/Home";
 import PostList from "./components/PostList";
@@ -19,34 +18,28 @@ const router = () => (
 				element: <App />,
 				children: [
 					{
-						path: "/",
-						element: <Layout />,
-						children: [
-							{
-								path: "*",
-								element: <NotFound />,
-							},
-							{
-								index: true,
-								element: <Home />,
-							},
-							{
-								path: "posts",
-								element: <PostList />,
-							},
-							{
-								path: "posts/:postId",
-								element: <PostDetail />,
-							},
-							{
-								path: "users/login",
-								element: <Login />,
-							},
-							{
-								path: "users/register",
-								element: <Register />,
-							},
-						],
+						path: "*",
+						element: <NotFound />,
+					},
+					{
+						index: true,
+						element: <Home />,
+					},
+					{
+						path: "posts",
+						element: <PostList />,
+					},
+					{
+						path: "posts/:postId",
+						element: <PostDetail />,
+					},
+					{
+						path: "users/login",
+						element: <Login />,
+					},
+					{
+						path: "users/register",
+						element: <Register />,
 					},
 				],
 			},
