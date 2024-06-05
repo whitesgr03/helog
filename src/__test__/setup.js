37890918d3dc66001f603380f7751bc1c3ff9af9
Weiss Bai <whitesgr03@gmail.com>
2024-06-05 09:05:10 +0800

    Add clearAllMocks to test setup

--- src/__test__/setup.js
+++ src/__test__/setup.js
@@ -1,9 +1,10 @@
-import { expect, afterEach } from "vitest";
+import { vi, expect, afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
 
 afterEach(() => {
+	vi.clearAllMocks();
 	cleanup();
 });
