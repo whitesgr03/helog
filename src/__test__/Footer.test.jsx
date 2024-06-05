5fc5f2dd30610819f8168887efebbcf878887e1c
Weiss Bai <whitesgr03@gmail.com>
2024-06-05 09:18:52 +0800

    Add mock Address component

--- src/__test__/Footer.test.jsx
+++ src/__test__/Footer.test.jsx
@@ -1,7 +1,9 @@
-import { describe, it, expect } from "vitest";
+import { vi, describe, it, expect } from "vitest";
 import { render } from "@testing-library/react";
 
 import Footer from "../components/Footer";
+
+vi.mock('../components/Address.jsx')
 
 describe("Footer component", () => {
 	it("should render correctly", () => {
