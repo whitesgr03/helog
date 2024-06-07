a02ecf58644704a4f34acbaa657920327caa025e
Weiss Bai <whitesgr03@gmail.com>
2024-06-07 18:36:51 +0800

    Refactor handleFetch
    After fetching success,  json data parsing  is returned.

--- src/utils/handleFetch.js
+++ src/utils/handleFetch.js
@@ -1,16 +1,7 @@
 const handleFetch = async (url, option) => {
 	const response = await fetch(url, { ...option });
 
-	const result = await response.json();
-
-	!result.success &&
-		(() => {
-			throw new Error("Fetch Error", {
-				cause: result.errors ?? result.message,
-			});
-		})();
-
-	return result.data;
+	return await response.json();
 };
 
 export default handleFetch;
