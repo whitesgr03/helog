840d5de8e2f17e1e270d1e6d5bd76fc80de9eb8c
Weiss Bai <whitesgr03@gmail.com>
2024-06-07 20:11:22 +0800

    Updated Header component
    Removed UserContext.
    
    Received user prop.
    
    Added dataset test id for test.
    
    Refactored handleCloseModel function.
    
    The handleClick function has been added to check if the element
    clicked by user has the specified dataset property. if yes,
    then close Settings component.
    
    The user id prop is passed to the Model components.

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -9,29 +9,31 @@ import image from "../styles/utils/image.module.css";
 import ChangeNameModel from "./ChangeNameModel";
 import DeleteAccountModel from "./DeleteAccountModel";
 
-import { UserContext } from "../contexts/UserContext";
-
 const defaultModel = {
 	changeName: false,
 	deleteAccount: false,
 };
-const Settings = ({ handleCloseSetting }) => {
+const Settings = ({ user, handleCloseSettings }) => {
 	const [model, setModel] = useState(defaultModel);
-	const { user } = UserContext();
 
 	const handleActiveModel = e => {
 		const { active } = e.target.dataset;
 		active && setModel({ ...model, [active]: true });
 	};
 
-	const handleCloseModel = e => {
-		!e
-			? setModel(defaultModel)
-			: e.target.dataset.close && setModel(defaultModel);
+	const handleCloseModel = () => setModel(defaultModel);
+
+	const handleClick = e => {
+		e.target.dataset.closeSetting && handleCloseSettings();
 	};
 
 	return (
-		<div className={blur} onClick={handleCloseSetting} data-close-setting>
+		<div
+			className={blur}
+			onClick={handleClick}
+			data-close-setting
+			data-testid={"blurBgc"}
+		>
 			<div className={style.settings} onClick={handleActiveModel}>
 				<button
 					type="button"
@@ -44,13 +46,13 @@ const Settings = ({ handleCloseSetting }) => {
 				<div className={style.container}>
 					<div className={style.avatarWrap}>
 						<div className={style.avatar}>
-							{user?.name && user.name.charAt(0).toUpperCase()}
+							{user && user.name.charAt(0).toUpperCase()}
 						</div>
 					</div>
 					<ul className={style.list}>
 						<li>
 							<strong className={style.title}>Name</strong>
-							{user?.name && <span>{user.name}</span>}
+							{user && <span>{user.name}</span>}
 							<button
 								className={style.changeBtn}
 								data-active="changeName"
@@ -60,7 +62,7 @@ const Settings = ({ handleCloseSetting }) => {
 						</li>
 						<li>
 							<strong className={style.title}>Email</strong>
-							{user?.email && <span>{user.email}</span>}
+							{user && <span>{user.email}</span>}
 						</li>
 						<li>
 							<strong className={style.title}>Delete</strong>
@@ -74,12 +76,16 @@ const Settings = ({ handleCloseSetting }) => {
 					</ul>
 				</div>
 				{model.changeName && (
-					<ChangeNameModel handleCloseModel={handleCloseModel} />
+					<ChangeNameModel
+						userId={user?._id}
+						handleCloseModel={handleCloseModel}
+					/>
 				)}
 				{model.deleteAccount && (
 					<DeleteAccountModel
+						userId={user?._id}
 						handleCloseModel={handleCloseModel}
-						handleCloseSetting={handleCloseSetting}
+						handleCloseSettings={handleCloseSettings}
 					/>
 				)}
 			</div>
@@ -88,7 +94,8 @@ const Settings = ({ handleCloseSetting }) => {
 };
 
 Settings.propTypes = {
-	handleCloseSetting: PropTypes.func,
+	user: PropTypes.object,
+	handleCloseSettings: PropTypes.func,
 };
 
 export default Settings;
