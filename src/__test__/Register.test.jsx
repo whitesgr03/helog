3e20d199c77a0283125a7516ee87cd6aa5cf9d15
Weiss Bai <whitesgr03@gmail.com>
2024-06-10 14:29:25 +0800

    Refactor Register component test

--- src/__test__/Register.test.jsx
+++ src/__test__/Register.test.jsx
@@ -8,6 +8,10 @@ import Register from "../components/Register";
 
 import handleFetch from "../utils/handleFetch";
 
+vi.mock("../components/Error.jsx", () => ({
+	default: () => <div>Error component</div>,
+}));
+
 vi.mock("../utils/handleFetch");
 
 /* eslint-disable react/prop-types */
@@ -24,7 +28,7 @@ const Providers = ({ children, ...props }) => {
 };
 
 describe("Register component", () => {
-	it("should render error messages if the validation field fails when submitting.", async () => {
+	it("should render errors for each input if 'handleValidFields' fails on submission.", async () => {
 		const user = userEvent.setup();
 		const mockOutContext = {
 			setToken: vi.fn(),
@@ -38,21 +42,65 @@ describe("Register component", () => {
 
 		const submitBtn = screen.getByRole("button", { name: "Register" });
 
+		let defaultErrorMessages = screen.getAllByText("Message placeholder");
+
+		expect(defaultErrorMessages.length).toEqual(4);
+
 		await user.click(submitBtn);
 
-		const defaultErrorMessages = screen.queryAllByText(
-			"Error message placeholder"
-		);
+		defaultErrorMessages = screen.queryAllByText("Message placeholder");
 
 		expect(defaultErrorMessages.length).toEqual(0);
 	});
-	it("should login successfully if the validation field is successful when submitting.", async () => {
+	it("should continue valid fields when entering again after submission failed.", async () => {
+		const user = userEvent.setup();
+		const mockOutContext = {
+			setToken: vi.fn(),
+		};
+
+		render(
+			<Providers {...mockOutContext}>
+				<Register />
+			</Providers>
+		);
+
+		const submitBtn = screen.getByRole("button", { name: "Register" });
+		const nameInput = screen.getByLabelText("Name");
+		const emailInput = screen.getByLabelText("Email");
+		const passwordInput = screen.getByLabelText("Password");
+		const confirmPasswordInput = screen.getByLabelText("Confirm Password");
+
+		await user.click(submitBtn);
+
+		let defaultErrorMessages = screen.queryAllByText("Message placeholder");
+
+		expect(defaultErrorMessages.length).toEqual(0);
+
+		await user.type(nameInput, "Example");
+		await user.type(emailInput, "example@gmail.com");
+		await user.type(passwordInput, "1!Qwerty");
+		await user.type(confirmPasswordInput, "1!Qwerty");
+
+		defaultErrorMessages = await screen.findAllByText(
+			"Message placeholder"
+		);
+
+		expect(defaultErrorMessages.length).toEqual(4);
+	});
+	it("should set token if the 'handleRegister' fetch result is successfully", async () => {
 		const user = userEvent.setup();
 		const mockOutletContext = {
 			setToken: vi.fn(),
 		};
 
-		handleFetch.mockReturnValueOnce({ data: [] });
+		const mockResult = {
+			success: true,
+			data: {
+				token: "exampleToken",
+			},
+		};
+
+		handleFetch.mockReturnValueOnce(mockResult);
 
 		render(
 			<Providers {...mockOutletContext}>
@@ -60,13 +108,14 @@ describe("Register component", () => {
 			</Providers>
 		);
 
-		const NameInput = screen.getByLabelText("Name");
+		const nameInput = screen.getByLabelText("Name");
 		const emailInput = screen.getByLabelText("Email");
 		const passwordInput = screen.getByLabelText("Password");
 		const confirmPasswordInput = screen.getByLabelText("Confirm Password");
+
 		const submitBtn = screen.getByRole("button", { name: "Register" });
 
-		await user.type(NameInput, "Example");
+		await user.type(nameInput, "Example");
 		await user.type(emailInput, "example@gmail.com");
 		await user.type(passwordInput, "1!Qwerty");
 		await user.type(confirmPasswordInput, "1!Qwerty");
@@ -76,71 +125,35 @@ describe("Register component", () => {
 		expect(handleFetch).toBeCalledTimes(1);
 		expect(mockOutletContext.setToken).toBeCalledTimes(1);
 	});
-	it("should continue validating fields when entering again after a failed submission validation.", async () => {
-		const user = userEvent.setup();
-		const mockOutContext = {
-			setToken: vi.fn(),
-		};
-
-		render(
-			<Providers {...mockOutContext}>
-				<Register />
-			</Providers>
-		);
-
-		const NameInput = screen.getByLabelText("Name");
-		const emailInput = screen.getByLabelText("Email");
-		const passwordInput = screen.getByLabelText("Password");
-		const confirmPasswordInput = screen.getByLabelText("Confirm Password");
-		const submitBtn = screen.getByRole("button", { name: "Register" });
-
-		await user.click(submitBtn);
-
-		let defaultErrorMessages = screen.queryAllByText(
-			"Error message placeholder"
-		);
-
-		expect(defaultErrorMessages.length).toEqual(0);
-
-		await user.type(NameInput, "Example");
-		await user.type(emailInput, "example@gmail.com");
-		await user.type(passwordInput, "1!Qwerty");
-		await user.type(confirmPasswordInput, "1!Qwerty");
-
-		defaultErrorMessages = await screen.findAllByText(
-			"Error message placeholder"
-		);
-
-		expect(defaultErrorMessages.length).toEqual(4);
-	});
-	it("should render error messages if handle fetch fails", async () => {
+	it("should render errors for each input if 'handleRegister' fetch result is fails.", async () => {
 		const user = userEvent.setup();
 		const mockOutletContext = {
 			setToken: vi.fn(),
 		};
 
-		handleFetch.mockImplementationOnce(() => {
-			throw new Error("Fetch Error", {
-				cause: [
-					{
-						field: "name",
-						message: "error",
-					},
-					{
-						field: "email",
-						message: "error",
-					},
-					{
-						field: "password",
-						message: "error",
-					},
-					{
-						field: "confirmPassword",
-						message: "error",
-					},
-				],
-			});
-		});
+		const mockResult = {
+			success: false,
+			errors: [
+				{
+					field: "name",
+					message: "error",
+				},
+				{
+					field: "email",
+					message: "error",
+				},
+				{
+					field: "password",
+					message: "error",
+				},
+				{
+					field: "confirmPassword",
+					message: "error",
+				},
+			],
+		};
+
+		handleFetch.mockReturnValueOnce(mockResult);
 
 		render(
 			<Providers {...mockOutletContext}>
@@ -148,13 +161,14 @@ describe("Register component", () => {
 			</Providers>
 		);
 
-		const NameInput = screen.getByLabelText("Name");
+		const nameInput = screen.getByLabelText("Name");
 		const emailInput = screen.getByLabelText("Email");
 		const passwordInput = screen.getByLabelText("Password");
 		const confirmPasswordInput = screen.getByLabelText("Confirm Password");
+
 		const submitBtn = screen.getByRole("button", { name: "Register" });
 
-		await user.type(NameInput, "Example");
+		await user.type(nameInput, "Example");
 		await user.type(emailInput, "example@gmail.com");
 		await user.type(passwordInput, "1!Qwerty");
 		await user.type(confirmPasswordInput, "1!Qwerty");
@@ -162,10 +176,82 @@ describe("Register component", () => {
 		await user.click(submitBtn);
 
 		const defaultErrorMessages = screen.queryAllByText(
-			"Error message placeholder"
+			"Message placeholder"
 		);
 
 		expect(defaultErrorMessages.length).toEqual(0);
 		expect(handleFetch).toBeCalledTimes(1);
+	});
+	it("should render the Error component if the 'handleRegister' fetch result is fails.", async () => {
+		const user = userEvent.setup();
+		const mockOutletContext = {
+			setToken: vi.fn(),
+		};
+
+		const mockResult = {
+			success: false,
+			message: "error",
+		};
+
+		handleFetch.mockReturnValueOnce(mockResult);
+
+		render(
+			<Providers {...mockOutletContext}>
+				<Register />
+			</Providers>
+		);
+
+		const nameInput = screen.getByLabelText("Name");
+		const emailInput = screen.getByLabelText("Email");
+		const passwordInput = screen.getByLabelText("Password");
+		const confirmPasswordInput = screen.getByLabelText("Confirm Password");
+
+		const submitBtn = screen.getByRole("button", { name: "Register" });
+
+		await user.type(nameInput, "Example");
+		await user.type(emailInput, "example@gmail.com");
+		await user.type(passwordInput, "1!Qwerty");
+		await user.type(confirmPasswordInput, "1!Qwerty");
+
+		await user.click(submitBtn);
+
+		const element = screen.getByText("Error component");
+
+		expect(handleFetch).toBeCalledTimes(1);
+		expect(element).toBeInTheDocument();
+	});
+	it("should render the Error component if an error is captured while performing 'handleRegister'.", async () => {
+		const user = userEvent.setup();
+		const mockOutletContext = {
+			setToken: vi.fn(),
+		};
+
+		handleFetch.mockImplementationOnce(() => {
+			throw new Error("Fetch Error");
+		});
+		render(
+			<Providers {...mockOutletContext}>
+				<Register />
+			</Providers>
+		);
+
+		const nameInput = screen.getByLabelText("Name");
+		const emailInput = screen.getByLabelText("Email");
+		const passwordInput = screen.getByLabelText("Password");
+		const confirmPasswordInput = screen.getByLabelText("Confirm Password");
+
+		const submitBtn = screen.getByRole("button", { name: "Register" });
+
+		await user.type(nameInput, "Example");
+		await user.type(emailInput, "example@gmail.com");
+		await user.type(passwordInput, "1!Qwerty");
+		await user.type(confirmPasswordInput, "1!Qwerty");
+
+		await user.click(submitBtn);
+
+		const element = screen.getByText("Error component");
+
+		expect(handleFetch).toBeCalledTimes(1);
+		expect(element).toBeInTheDocument();
 	});
 });
