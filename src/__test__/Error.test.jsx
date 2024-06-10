9de655cace397c8093096b51350f573f75005001
Weiss Bai <whitesgr03@gmail.com>
2024-06-10 20:20:02 +0800

    Updated test

--- src/__test__/Error.test.jsx
+++ src/__test__/Error.test.jsx
@@ -13,7 +13,7 @@ describe("Error component", () => {
 		expect(element).toBeInTheDocument();
 	});
 	it("should render default message if type of message prop is not string", () => {
-		render(<Error message={123} />);
+		render(<Error message={{}} />);
 
 		const element = screen.getByText(
 			"Please come back later or if you have any questions, please contact us."
