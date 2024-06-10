e1b430024f23f689ad0d1b53fcbff1575820fef9
Weiss Bai <whitesgr03@gmail.com>
2024-06-10 14:28:57 +0800

    Refactor Login component test

--- src/__test__/Login.test.jsx
+++ src/__test__/Login.test.jsx
@@ -1,5 +1,4 @@
-import { vi } from "vitest";
-import { describe, it, expect } from "vitest";
+import { vi, describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
@@ -7,6 +6,10 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Login from "../components/Login";
 
 import handleFetch from "../utils/handleFetch";
+
+vi.mock("../components/Error.jsx", () => ({
+	default: () => <div>Error component</div>,
+}));
 
 vi.mock("../utils/handleFetch");
 
@@ -24,7 +27,7 @@ const Providers = ({ children, ...props }) => {
 };
 
 describe("Login component", () => {
-	it("should render error messages if the validation field fails when submitting.", async () => {
+	it("should render errors for each input if 'handleValidFields' fails on submission.", async () => {
 		const user = userEvent.setup();
 		const mockOutContext = {
 			setToken: vi.fn(),
@@ -38,21 +41,61 @@ describe("Login component", () => {
 
 		const submitBtn = screen.getByRole("button", { name: "Login" });
 
+		let defaultErrorMessages = screen.getAllByText("Message placeholder");
+
+		expect(defaultErrorMessages.length).toEqual(2);
+
 		await user.click(submitBtn);
 
-		const defaultErrorMessages = screen.queryAllByText(
-			"Error message placeholder"
-		);
+		defaultErrorMessages = screen.queryAllByText("Message placeholder");
 
 		expect(defaultErrorMessages.length).toEqual(0);
 	});
-	it("should login successfully if the validation field is successful when submitting.", async () => {
+	it("should continue valid fields when entering again after submission failed.", async () => {
+		const user = userEvent.setup();
+		const mockOutContext = {
+			setToken: vi.fn(),
+		};
+
+		render(
+			<Providers {...mockOutContext}>
+				<Login />
+			</Providers>
+		);
+
+		const submitBtn = screen.getByRole("button", { name: "Login" });
+		const emailInput = screen.getByLabelText("Email");
+		const passwordInput = screen.getByLabelText("Password");
+
+		await user.click(submitBtn);
+
+		let defaultErrorMessages = screen.queryAllByText("Message placeholder");
+
+		expect(defaultErrorMessages.length).toEqual(0);
+
+		await user.type(emailInput, "example@gmail.com");
+		await user.type(passwordInput, "1!Qwerty");
+
+		defaultErrorMessages = await screen.findAllByText(
+			"Message placeholder"
+		);
+
+		expect(defaultErrorMessages.length).toEqual(2);
+	});
+	it("should set token if the 'handleLogin' fetch result is successfully", async () => {
 		const user = userEvent.setup();
 		const mockOutletContext = {
 			setToken: vi.fn(),
 		};
 
-		handleFetch.mockReturnValueOnce({ data: [] });
+		const mockResult = {
+			success: true,
+			data: {
+				token: "exampleToken",
+			},
+		};
+
+		handleFetch.mockReturnValueOnce(mockResult);
 
 		render(
 			<Providers {...mockOutletContext}>
@@ -72,59 +115,64 @@ describe("Login component", () => {
 		expect(handleFetch).toBeCalledTimes(1);
 		expect(mockOutletContext.setToken).toBeCalledTimes(1);
 	});
-	it("should continue validating fields when entering again after a failed submission validation.", async () => {
-		const user = userEvent.setup();
-		const mockOutContext = {
-			setToken: vi.fn(),
-		};
-
-		render(
-			<Providers {...mockOutContext}>
-				<Login />
-			</Providers>
-		);
-
-		const submitBtn = screen.getByRole("button", { name: "Login" });
-		const emailInput = screen.getByLabelText("Email");
-		const passwordInput = screen.getByLabelText("Password");
-
-		await user.click(submitBtn);
-
-		let defaultErrorMessages = screen.queryAllByText(
-			"Error message placeholder"
-		);
-
-		expect(defaultErrorMessages.length).toEqual(0);
-
-		await user.type(emailInput, "example@gmail.com");
-		await user.type(passwordInput, "1!Qwerty");
-
-		defaultErrorMessages = await screen.findAllByText(
-			"Error message placeholder"
-		);
-
-		expect(defaultErrorMessages.length).toEqual(2);
-	});
-	it("should render error messages if handle fetch fails", async () => {
+	it("should render errors for each input if 'handleLogin' fetch result is fails.", async () => {
 		const user = userEvent.setup();
 		const mockOutletContext = {
 			setToken: vi.fn(),
 		};
 
-		handleFetch.mockImplementationOnce(() => {
-			throw new Error("Fetch Error", {
-				cause: [
-					{
-						field: "email",
-						message: "error",
-					},
-					{
-						field: "password",
-						message: "error",
-					},
-				],
-			});
-		});
+		const mockResult = {
+			success: false,
+			errors: [
+				{
+					field: "email",
+					message: "error",
+				},
+				{
+					field: "password",
+					message: "error",
+				},
+			],
+		};
+
+		handleFetch.mockReturnValueOnce(mockResult);
+
+		render(
+			<Providers {...mockOutletContext}>
+				<Login />
+			</Providers>
+		);
+
+		const emailInput = screen.getByLabelText("Email");
+		const passwordInput = screen.getByLabelText("Password");
+		const submitBtn = screen.getByRole("button", { name: "Login" });
+
+		await user.type(emailInput, "example@gmail.com");
+		await user.type(passwordInput, "1!Qwerty");
+
+		let defaultErrorMessages = screen.getAllByText("Message placeholder");
+
+		expect(defaultErrorMessages.length).toEqual(2);
+
+		await user.click(submitBtn);
+
+		defaultErrorMessages = screen.queryAllByText("Message placeholder");
+
+		expect(defaultErrorMessages.length).toEqual(0);
+		expect(handleFetch).toBeCalledTimes(1);
+	});
+	it("should render the Error component if the 'handleLogin' fetch result is fails.", async () => {
+		const user = userEvent.setup();
+		const mockOutletContext = {
+			setToken: vi.fn(),
+		};
+
+		const mockResult = {
+			success: false,
+			message: "error",
+		};
+
+		handleFetch.mockReturnValueOnce(mockResult);
 
 		render(
 			<Providers {...mockOutletContext}>
@@ -141,11 +189,41 @@ describe("Login component", () => {
 
 		await user.click(submitBtn);
 
-		const defaultErrorMessages = screen.queryAllByText(
-			"Error message placeholder"
+		const element = screen.getByText("Error component");
+
+		expect(handleFetch).toBeCalledTimes(1);
+		expect(element).toBeInTheDocument();
+	});
+	it("should render the Error component if an error is captured while performing 'handleLogin'.", async () => {
+		const user = userEvent.setup();
+		const mockOutletContext = {
+			setToken: vi.fn(),
+		};
+
+		handleFetch.mockImplementationOnce(() => {
+			throw new Error("Fetch Error");
+		});
+		render(
+			<Providers {...mockOutletContext}>
+				<Login />
+			</Providers>
 		);
 
-		expect(defaultErrorMessages.length).toEqual(0);
+		const emailInput = screen.getByLabelText("Email");
+		const passwordInput = screen.getByLabelText("Password");
+		const submitBtn = screen.getByRole("button", { name: "Login" });
+
+		await user.type(emailInput, "example@gmail.com");
+		await user.type(passwordInput, "1!Qwerty");
+
+		await user.click(submitBtn);
+
+		const element = screen.getByText("Error component");
+
 		expect(handleFetch).toBeCalledTimes(1);
+		expect(element).toBeInTheDocument();
 	});
 });
+
+// 解決每次輸入時都會有多次 test 執行的問題
+// 再從 header 開始
