eaab4b1eb7b55ba0f3a2df91b4e23c34f5938b7d
Weiss Bai <whitesgr03@gmail.com>
2024-06-17 07:22:05 +0800

    Add CSP Nonce based

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { randomBytes } from "node:crypto";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	html: {
+		cspNonce: randomBytes(16).toString("base64"),
+	},
 	test: {
 		globals: true,
 		environment: "jsdom",
