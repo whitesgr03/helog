20422dc0ca5dce2aa3b5c67e6c3d3dfba2d72f9d
Weiss Bai <whitesgr03@gmail.com>
2024-07-06 18:39:36 +0800

    Added @vitejs/plugin-basic-ssl to use https TLS

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { randomBytes } from "node:crypto";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), basicSsl()],
 	html: {
 		cspNonce: randomBytes(16).toString("base64"),
 	},
