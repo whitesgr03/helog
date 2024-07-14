61f9c76da433301708a0122753b70aec1dc1263f
Weiss Bai <whitesgr03@gmail.com>
2024-07-15 00:59:14 +0800

    Added OutletContext to get error state

--- src/components/layout/Error.jsx
+++ src/components/layout/Error.jsx
@@ -1,13 +1,22 @@
+// Packages
+import { useOutletContext } from "react-router-dom";
 import PropTypes from "prop-types";
 
-import style from "../styles/Error.module.css";
-import image from "../styles/utils/image.module.css";
+// Styles
+import style from "../../styles/layout/Error.module.css";
+import image from "../../styles/utils/image.module.css";
 
+// Variable
 const defaultMessage =
 	"Please come back later or if you have any questions, please contact us.";
 
 const Error = ({ message = null }) => {
-	const errorMessage = typeof message === "string" ? message : defaultMessage;
+	const { error } = useOutletContext();
+
+	const errorMessage =
+		(typeof message === "string" && message) || defaultMessage;
+
+	console.error(message || error);
 
 	return (
 		<div className={style.error}>
