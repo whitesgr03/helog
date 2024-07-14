ce14dbf99199f87fffd8ffe4bf6e28103af60bc5
Weiss Bai <whitesgr03@gmail.com>
2024-07-14 19:48:41 +0800

    Created handleReply function

---
+++ src/utils/handleReply.js
@@ -0,0 +1,37 @@
+import handleFetch from "./handleFetch";
+
+const url = `${import.meta.env.VITE_RESOURCE_ORIGIN}/blog/replies`;
+
+const createReply = async ({ token, data }) => {
+	const options = {
+		method: "POST",
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	};
+	return await handleFetch(url, options);
+};
+const updateReply = async ({ token, data, replyId }) => {
+	const options = {
+		method: "PUT",
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	};
+	return await handleFetch(`${url}/${replyId}`, options);
+};
+const deleteReply = async ({ token, replyId }) => {
+	const options = {
+		method: "DELETE",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+	return await handleFetch(`${url}/${replyId}`, options);
+};
+
+export { createReply, updateReply, deleteReply };
