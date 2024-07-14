0f347f613d7abbac6817b2c2a302ccb8c8be6387
Weiss Bai <whitesgr03@gmail.com>
2024-07-14 21:28:32 +0800

    Remove postList component and module css

---
+++ src/components/post/PostList.jsx
@@ -0,0 +1,15 @@
+// Styles
+import style from "../../styles/post/PostList.module.css";
+
+// Components
+import Posts from "./Posts";
+
+const PostList = () => {
+	return (
+		<div className={style.postList}>
+			<Posts />
+		</div>
+	);
+};
+
+export default PostList;
