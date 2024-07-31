cbe7eae66d19c944cbdce56f41901adc38adc114
Weiss Bai <whitesgr03@gmail.com>
2024-07-31 15:48:38 +0800

    Updated UserProvider
    Added useEffect to get url darkTheme parameter.
    
    Renamed handleSwitchColorTheme to handleColorTheme.
    
    Added ignore ref for Outlet context state
    
    Renamed Outlet context states.

--- src/components/UserProvider.jsx
+++ src/components/UserProvider.jsx
@@ -1,6 +1,6 @@
 // Packages
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 // Styles
 import style from "../styles/UserProvider.module.css";
@@ -9,20 +9,26 @@ import style from "../styles/UserProvider.module.css";
 import handleColorScheme from "../utils/handleColorScheme";
 
 const UserProvider = () => {
+	const [searchParams] = useSearchParams();
 	const [user, setUser] = useState(null);
 	const [error, setError] = useState(null);
 	const [refreshToken, setRefreshToken] = useState(null);
 	const [accessToken, setAccessToken] = useState(null);
-
 	const [darkTheme, setDarkTheme] = useState(
 		JSON.parse(localStorage.getItem("heLog.darkTheme")) ?? handleColorScheme
 	);
+	const ignore = useRef(false);
 
-	const handleSwitchColorTheme = () => {
+	const handleColorTheme = () => {
 		localStorage.setItem("heLog.darkTheme", JSON.stringify(!darkTheme));
 		setDarkTheme(!darkTheme);
 	};
 
+	useEffect(() => {
+		const darkTheme = searchParams.get("darkTheme") === "true";
+		darkTheme !== null &&
+			localStorage.setItem("heLog.darkTheme", JSON.stringify(darkTheme));
+	}, [searchParams]);
 	return (
 		<div
 			className={` ${darkTheme ? "dark" : ""} ${style.user_provider}`}
@@ -30,16 +36,17 @@ const UserProvider = () => {
 		>
 			<Outlet
 				context={{
-					user,
-					setUser,
-					error,
-					setError,
-					refreshToken,
-					setRefreshToken,
-					accessToken,
-					setAccessToken,
 					darkTheme,
-					handleSwitchColorTheme,
+					user,
+					refreshToken,
+					accessToken,
+					error,
+					ignore,
+					onUser: setUser,
+					onError: setError,
+					onColorTheme: handleColorTheme,
+					onAccessToken: setAccessToken,
+					onRefreshToken: setRefreshToken,
 				}}
 			/>
 		</div>
