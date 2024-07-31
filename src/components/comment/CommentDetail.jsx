c894d16cd421575083a3017ab17eaffad574bbf2
Weiss Bai <whitesgr03@gmail.com>
2024-07-31 17:23:18 +0800

    Updated commentDetail component
    Deleted error state and component.
    
    Added newAccessToken variable to store handleExChangeToken return
    value.
    
    Added onAlert into handleDelete to render error message.
    
    Use formatDistanceToNow to replace formatDistanceToNowStrict.
    
    The p tag in the content class uses dangerouslySetInnerHTML to
    render escaped comment content.
    
    Added user to conditional for render "replyList".

--- src/components/comment/CommentDetail.jsx
+++ src/components/comment/CommentDetail.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import { useOutletContext } from "react-router-dom";
-import { formatDistanceToNowStrict } from "date-fns";
+import { formatDistanceToNow } from "date-fns";
 
 // Styles
 import style from "../../styles/comment/CommentDetail.module.css";
@@ -10,7 +10,6 @@ import image from "../../styles/utils/image.module.css";
 
 // Components
 import CommentBox from "./CommentBox";
-import Error from "../layout/Error";
 
 // Utils
 import { createReply, updateReply, deleteReply } from "../../utils/handleReply";
@@ -32,14 +31,13 @@ const CommentDetail = ({
 	const {
 		user,
 		accessToken,
-		refreshToken,
 		handleVerifyTokenExpire,
 		handleExChangeToken,
+		onAlert,
 	} = useOutletContext();
 	const [showReplies, setShowReplies] = useState(false);
 	const [showReplyCommentBox, setShowReplyCommentBox] = useState(false);
 	const [showEditBox, setShowEditBox] = useState(false);
-	const [error, setError] = useState(null);
 
 	const handleShowReplies = () => setShowReplies(!showReplies);
 	const handleShowEditBox = () => {
@@ -49,8 +47,8 @@ const CommentDetail = ({
 		setShowReplyCommentBox(!showReplyCommentBox);
 
 	const handleCreateReply = async fields => {
-		const isTokenExpire = await handleVerifyTokenExpire(accessToken);
-		isTokenExpire && (await handleExChangeToken(refreshToken));
+		const isTokenExpire = await handleVerifyTokenExpire();
+		const newAccessToken = isTokenExpire && (await handleExChangeToken());
 
 		const data = {
 			...fields,
@@ -61,7 +59,7 @@ const CommentDetail = ({
 		replyId && (data.reply = replyId);
 
 		const result = await createReply({
-			token: accessToken,
+			token: newAccessToken || accessToken,
 			data,
 		});
 
@@ -69,11 +67,11 @@ const CommentDetail = ({
 	};
 
 	const handleDelete = async () => {
-		const isTokenExpire = await handleVerifyTokenExpire(accessToken);
-		isTokenExpire && handleExChangeToken(refreshToken);
+		const isTokenExpire = await handleVerifyTokenExpire();
+		const newAccessToken = isTokenExpire && (await handleExChangeToken());
 
 		const obj = {
-			token: accessToken,
+			token: newAccessToken || accessToken,
 		};
 
 		replyId ? (obj.replyId = replyId) : (obj.commentId = commentId);
@@ -86,15 +84,15 @@ const CommentDetail = ({
 			? replyId
 				? await handleGetReplies()
 				: await handleGetComments()
-			: setError(result.message);
+			: onAlert({ message: result.message, error: true });
 	};
 
 	const handleUpdate = async fields => {
-		const isTokenExpire = await handleVerifyTokenExpire(accessToken);
-		isTokenExpire && handleExChangeToken(refreshToken);
+		const isTokenExpire = await handleVerifyTokenExpire();
+		const newAccessToken = isTokenExpire && (await handleExChangeToken());
 
 		const obj = {
-			token: accessToken,
+			token: newAccessToken || accessToken,
 			data: {
 				...fields,
 			},
@@ -110,150 +108,133 @@ const CommentDetail = ({
 	};
 
 	return (
-		<>
-			{error ? (
-				<Error message={error} />
-			) : (
-				<li id={`item-${comment._id}`}>
-					<div
-						className={`${style.container} ${
-							isPostAuthor ? style.author : ""
-						} ${isCommentAuthor ? style.user : ""}`}
-						data-testid="container"
-					>
-						{!isDeleted && (
-							<div className={style.buttonWrap}>
-								{isPostAuthor && (
-									<em
-										className={`${
-											isPostAuthor ? style.highlight : ""
-										}`}
-									>
-										POST AUTHOR
-									</em>
-								)}
-								{(isCommentAuthor || user.isAdmin) && (
-									<div className={style.commentButton}>
-										<button onClick={handleDelete}>
-											<span
-												className={`${image.icon} ${style.delete}`}
-											/>
-										</button>
-
-										<button onClick={handleShowEditBox}>
-											<span
-												className={`${image.icon} ${style.edit}`}
-											/>
-										</button>
-									</div>
-								)}
-							</div>
-						)}
-						<div className={style.infoWrap}>
-							<div className={style.info}>
-								<div className={style.avatar}>
-									{!isDeleted && (
-										<>
-											{comment.author.name
-												.charAt(0)
-												.toUpperCase()}
-										</>
-									)}
-								</div>
-								<strong
-									title={
-										!isDeleted ? comment?.author.name : ""
-									}
-								>
-									{!isDeleted
-										? comment?.author.name
-										: "[deleted]"}
-								</strong>
-							</div>
-							<div className={style.time}>
-								{`${formatDistanceToNowStrict(
-									comment.lastModified
-								)} ago ${
-									comment.createdAt !==
-										comment.lastModified && "(edited)"
+		<li id={`item-${comment._id}`}>
+			<div
+				className={`${style.container} ${
+					isPostAuthor ? style.author : ""
+				} ${isCommentAuthor ? style.user : ""}`}
+				data-testid="container"
+			>
+				{!isDeleted && (
+					<div className={style.buttonWrap}>
+						{isPostAuthor && (
+							<em
+								className={`${
+									isPostAuthor ? style.highlight : ""
 								}`}
-							</div>
-						</div>
-
-						{!isDeleted && showEditBox ? (
-							<div className={style.editBoxWrap}>
-								<CommentBox
-									submitBtn={"Update"}
-									onGetComments={
-										replyId
-											? handleGetReplies
-											: handleGetComments
-									}
-									onCreateComment={handleUpdate}
-									onCloseCommentBox={handleShowEditBox}
-									defaultValue={comment.content}
-								/>
-							</div>
-						) : (
-							<div className={style.content}>
-								{!isDeleted && comment.reply && (
-									<a href={`#item-${comment.reply._id}`}>
-										@
-										{comment.reply.deleted
-											? "[delete]"
-											: comment.reply.author.name}
-									</a>
-								)}
-								<p>{comment.content}</p>
-							</div>
+							>
+								POST AUTHOR
+							</em>
 						)}
+						{(isCommentAuthor || user?.isAdmin) && (
+							<div className={style.commentButton}>
+								<button onClick={handleDelete}>
+									<span
+										className={`${image.icon} ${style.delete}`}
+									/>
+								</button>
 
-						{(replyList || (!isDeleted && !showEditBox)) && (
-							<div className={style.buttonWrap}>
-								{replyList && (
-									<button
-										className={style.replyListBtn}
-										onClick={handleShowReplies}
-									>
-										<span
-											className={`${image.icon} ${
-												style.replyIcon
-											} ${
-												showReplies ? style.active : ""
-											}`}
-											data-testid="commentIcon"
-										/>
-										{replyList.length}
-									</button>
-								)}
-								{!isDeleted && !showEditBox && (
-									<button
-										className={style.addReplyBtn}
-										onClick={handleShowReplyCommentBox}
-									>
-										Reply
-									</button>
-								)}
+								<button onClick={handleShowEditBox}>
+									<span
+										className={`${image.icon} ${style.edit}`}
+									/>
+								</button>
 							</div>
 						)}
 					</div>
-
-					{!isDeleted && showReplyCommentBox && (
-						<div className={style.commentBoxWrap}>
-							<CommentBox
-								submitBtn={"Reply"}
-								onGetComments={handleGetReplies}
-								onCreateComment={handleCreateReply}
-								onCloseCommentBox={handleShowReplyCommentBox}
-							/>
+				)}
+				<div className={style.infoWrap}>
+					<div className={style.info}>
+						<div className={style.avatar}>
+							{!isDeleted && (
+								<>
+									{comment.author.name
+										.charAt(0)
+										.toUpperCase()}
+								</>
+							)}
 						</div>
-					)}
-					{showReplies && (
-						<ul className={style.replies}>{children}</ul>
-					)}
-				</li>
+						<strong title={!isDeleted ? comment?.author.name : ""}>
+							{!isDeleted ? comment?.author.name : "[deleted]"}
+						</strong>
+					</div>
+					<div className={style.time}>
+						{`${formatDistanceToNow(comment.lastModified)} ago `}
+						{comment.createdAt !== comment.lastModified &&
+							"(edited)"}
+					</div>
+				</div>
+
+				{!isDeleted && showEditBox ? (
+					<div className={style.editBoxWrap}>
+						<CommentBox
+							submitBtn={"Update"}
+							onGetComments={
+								replyId ? handleGetReplies : handleGetComments
+							}
+							onCreateComment={handleUpdate}
+							onCloseCommentBox={handleShowEditBox}
+							defaultValue={comment.content}
+						/>
+					</div>
+				) : (
+					<div className={style.content}>
+						{!isDeleted && comment.reply && (
+							<a href={`#item-${comment.reply._id}`}>
+								@
+								{comment.reply.deleted
+									? "[delete]"
+									: comment.reply.author.name}
+							</a>
+						)}
+						<p
+							dangerouslySetInnerHTML={{
+								__html: comment.content,
+							}}
+						/>
+					</div>
+				)}
+
+				{user && (replyList || (!isDeleted && !showEditBox)) && (
+					<div className={style.buttonWrap}>
+						{replyList && (
+							<button
+								className={style.replyListBtn}
+								onClick={handleShowReplies}
+							>
+								<span
+									className={`${image.icon} ${
+										style.replyIcon
+									} ${showReplies ? style.active : ""}`}
+									data-testid="commentIcon"
+								/>
+								{replyList.length}
+							</button>
+						)}
+						{!isDeleted && !showEditBox && (
+							<button
+								className={style.addReplyBtn}
+								onClick={handleShowReplyCommentBox}
+							>
+								Reply
+							</button>
+						)}
+					</div>
+				)}
+			</div>
+
+			{!isDeleted && showReplyCommentBox && (
+				<div className={style.commentBoxWrap}>
+					<CommentBox
+						submitBtn={"Reply"}
+						onGetComments={handleGetReplies}
+						onCreateComment={handleCreateReply}
+						onCloseCommentBox={handleShowReplyCommentBox}
+					/>
+				</div>
 			)}
-		</>
+			{showReplies && <ul className={style.replies}>{children}</ul>}
+		</li>
 	);
 };
 
