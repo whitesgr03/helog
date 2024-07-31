65c266a121d67f686265bbf54337dd919d6d58bd
Weiss Bai <whitesgr03@gmail.com>
2024-07-31 16:13:16 +0800

    Updated Posts component
    Use useEffect to get posts instead of useFetch.
    
    A new div with info class adds to render post author and
    last modified time.
    
    The link of h3 uses dangerouslySetInnerHTML to render escaped post
    title.

--- src/components/post/Posts.jsx
+++ src/components/post/Posts.jsx
@@ -1,6 +1,7 @@
 // Packages
+import { useState, useEffect } from "react";
 import { format } from "date-fns";
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 import PropTypes from "prop-types";
 
 // Styles
@@ -12,49 +13,86 @@ import Loading from "../layout/Loading";
 import Error from "../layout/Error";
 
 // Utils
-import useFetch from "../../hooks/useFetch";
-
-const url = `${import.meta.env.VITE_RESOURCE_ORIGIN}/blog/posts`;
+import { getPosts } from "../../utils/handlePost";
 
 const Posts = ({ limit = 0 }) => {
-	const { data, error, loading } = useFetch(`${url}?limit=${limit}`);
+	const { user } = useOutletContext();
+	const [posts, setPosts] = useState([]);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-	const items = data
-		? data.map(
-				post =>
-					post?.content !== "" && (
-						<li key={post._id}>
-							<Link to={`/posts/${post._id}`}>
-								<div className={image.content}>
-									{post.mainImageUrl ? (
-										<img
-											src={post.mainImageUrl}
-											alt={`${post.title} main image`}
-										/>
-									) : (
-										<div className={style.emptyImageWrap}>
-											{"( Empty Main Image )"}
-										</div>
-									)}
+	const items = posts.map(
+		post =>
+			post?.content !== "" && (
+				<li key={post._id}>
+					<div className={style.info}>
+						<strong
+							className={style.dateTime}
+							title={post.author.name}
+						>
+							{post.author.name}
+						</strong>
+						<em>
+							{format(
+								new Date(post.createdAt).getTime() ===
+									new Date(post.lastModified).getTime()
+									? post.createdAt
+									: post.lastModified,
+								"MMMM d, y"
+							)}
+						</em>
+					</div>
+
+					<Link to={`/posts/${post._id}`}>
+						<div className={image.content}>
+							{post.mainImageUrl ? (
+								<img
+									src={post.mainImageUrl}
+									alt={`${post.title} main image`}
+								/>
+							) : (
+								<div className={style.emptyImageWrap}>
+									{"( Empty Main Image )"}
 								</div>
-							</Link>
+							)}
+						</div>
+					</Link>
 
-							<div className={style.container}>
-								<strong className={style.dateTime}>
-									{format(post.createdAt, "MMMM d, y")}
-								</strong>
-								<Link to={`/posts/${post._id}`}>
-									<h3 className={style.title}>
-										{post.title
-											? post.title
-											: "( Empty Title )"}
-									</h3>
-								</Link>
-							</div>
-						</li>
-					)
-		  )
-		: [];
+					<Link to={`/posts/${post._id}`}>
+						<h3
+							className={style.title}
+							title={post.title}
+							dangerouslySetInnerHTML={{
+								__html: `${
+									post.title ? post.title : "( Empty Title )"
+								}`,
+							}}
+						/>
+					</Link>
+				</li>
+			)
+	);
+
+	useEffect(() => {
+		const controller = new AbortController();
+		const { signal } = controller;
+
+		const handleGetPosts = async () => {
+			const result = await getPosts({ limit, signal });
+
+			const handleResult = () => {
+				result.success
+					? setPosts(result.data)
+					: setError(result.message);
+				setLoading(false);
+			};
+
+			result && handleResult();
+		};
+
+		handleGetPosts();
+		return () => controller.abort();
+	}, [user, limit]);
 
 	return (
 		<>
