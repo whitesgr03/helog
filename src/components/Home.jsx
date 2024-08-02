83713dfce613074f421d74402e9f07c4c10ae79a
Weiss Bai <whitesgr03@gmail.com>
2024-08-02 13:30:49 +0800

    Updated content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,15 +23,15 @@ const Home = () => {
 				<div className={style.content}>
 					<h2 className={style.title}>A Daily Stories For Reader</h2>
 					<p>
-						Lorem ipsum dolor sit amet consectetur, adipisicing
-						elit. Odio, veniam voluptatem veritatis maxime iste
-						rerum quo blanditiis cum ea nulla officiis neque
-						molestiae voluptate qui adipisci velit magnam
-						consequatur. Quo. Quia voluptas consequuntur assumenda
-						ut totam quaerat explicabo eum nemo? Eveniet iste neque
-						eligendi eius provident doloremque magnam maiores culpa
-						repudiandae ipsam. Repudiandae illum facilis, omnis qui
-						expedita debitis ea.
+						The personal blog is an ongoing online diary or
+						commentary written by an individual, rather than a
+						corporation or organization. While the vast majority of
+						personal blogs attract very few readers, other than the
+						blogger's immediate family and friends, a small number
+						of personal blogs have become popular, to the point that
+						they have attracted lucrative advertising sponsorship. A
+						tiny number of personal bloggers have become famous,
+						both in the online community and in the real world.
 					</p>
 					<Link to={`/posts`} className={style.link}>
 						<span>Latest </span>
