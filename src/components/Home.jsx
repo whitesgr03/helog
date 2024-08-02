21b6496d6fddf269eb4d29cb3084600f4e4754fb
Weiss Bai <whitesgr03@gmail.com>
2024-08-02 13:35:38 +0800

    Updated escape character

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,11 +27,12 @@ const Home = () => {
 						commentary written by an individual, rather than a
 						corporation or organization. While the vast majority of
 						personal blogs attract very few readers, other than the
-						blogger's immediate family and friends, a small number
-						of personal blogs have become popular, to the point that
-						they have attracted lucrative advertising sponsorship. A
-						tiny number of personal bloggers have become famous,
-						both in the online community and in the real world.
+						blogger&apos;s immediate family and friends, a small
+						number of personal blogs have become popular, to the
+						point that they have attracted lucrative advertising
+						sponsorship. A tiny number of personal bloggers have
+						become famous, both in the online community and in the
+						real world.
 					</p>
 					<Link to={`/posts`} className={style.link}>
 						<span>Latest </span>
