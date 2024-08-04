bcf0f8721a549029078bc812e5fc972c90c8a842
Weiss Bai <whitesgr03@gmail.com>
2024-08-04 18:53:31 +0800

    Created Model component and module css

---
+++ src/components/layout/Model.jsx
@@ -0,0 +1,37 @@
+// Packages
+import PropTypes from "prop-types";
+
+// Styles
+import style from "../../styles/layout/Model.module.css";
+import image from "../../styles/utils/image.module.css";
+import button from "../../styles/utils/button.module.css";
+import { blurWindow } from "../../styles/utils/bgc.module.css";
+
+const Model = ({ onModel, model }) => {
+	const handleCloseModel = e => e.target.dataset.closeModel && onModel(null);
+	return (
+		<div
+			className={`${blurWindow} ${style.model}`}
+			onClick={handleCloseModel}
+			data-close-model
+		>
+			<div className={style.content}>
+				<button
+					type="button"
+					className={button.closeBtn}
+					data-close-model
+				>
+					<span className={`${image.icon} ${button.close}`} />
+				</button>
+				{model}
+			</div>
+		</div>
+	);
+};
+
+Model.propTypes = {
+	onModel: PropTypes.func,
+	model: PropTypes.object,
+};
+
+export default Model;
