58522a2a7506c3fedbde44efc7c49f05d7f4f32c
Weiss Bai <whitesgr03@gmail.com>
2024-12-25 00:41:43 +0800

    feat: get error message from state instead of outlet context

--- src/components/layout/Error.jsx
+++ src/components/layout/Error.jsx
@@ -1,36 +1,35 @@
 // Packages
-import { useOutletContext, Link } from "react-router-dom";
-import PropTypes from "prop-types";
+import { Link, useLocation } from "react-router-dom";
 
 // Styles
 import style from "../../styles/layout/Error.module.css";
 import image from "../../styles/utils/image.module.css";
 
-const Error = ({ message = null }) => {
-	const { error } = useOutletContext();
-
-	const errorMessage =
-		(typeof message === "string" && message) ||
-		"Please come back later or if you have any questions, please contact us.";
-
-	console.error(message || error);
+	const { state } = useLocation();
 
 	return (
 		<div className={style.error}>
 			<span className={`${image.icon} ${style.alert}`} />
 			<div className={style.message}>
 				<p>Our apologies, there has been an error.</p>
-				<p>{errorMessage}</p>
+				{state?.customMessage ? (
+					<p>{state?.error}</p>
+				) : (
+					<p>
+						Please come back later, or if you have any questions,
+						contact us.
+					</p>
+				)}
 			</div>
+			{state?.previousPath && (
+				<Link to={state.previousPath} className={style.link}>
+					Go Back
+				</Link>
+			)}
 			<Link to="/" className={style.link}>
 				Back to Home Page.
 			</Link>
 		</div>
 	);
 };
-
-Error.propTypes = {
-	message: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-};
-
 export default Error;
