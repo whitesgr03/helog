3ddacb1cb759a2896a2a4421d08f6a34f1022815
Weiss Bai <whitesgr03@gmail.com>
2024-12-24 23:58:32 +0800

    docs: remove basicSsl

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import { randomBytes } from "node:crypto";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), basicSsl()],
+	plugins: [react()],
 	html: {
 		cspNonce: randomBytes(16).toString("base64"),
 	},
