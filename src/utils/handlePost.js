d67ee7f980dd613782bf97319e33d72ee0f1d307
Weiss Bai <whitesgr03@gmail.com>
2024-12-25 18:27:37 +0800

    feat: all functions change to set credentials instead of headers Authorization

--- src/utils/handlePost.js
+++ src/utils/handlePost.js
@@ -4,16 +4,18 @@ const url = `${import.meta.env.VITE_RESOURCE_URL}/blog/posts`;
 
 export const getPosts = async ({ limit, signal }) => {
 	const options = {
-		method: "GET",
+		method: 'GET',
 		signal,
+		credentials: 'include',
 	};
 	return await handleFetch(`${url}?limit=${limit}`, options);
 };
 
 export const getPostDetail = async ({ postId, signal }) => {
 	const options = {
-		method: "GET",
+		method: 'GET',
 		signal,
+		credentials: 'include',
 	};
 	return await handleFetch(`${url}/${postId}`, options);
 };
