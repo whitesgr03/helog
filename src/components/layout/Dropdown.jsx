56e26b575d41edda0d88c7fbf3388a23b231c8f4
Weiss Bai <whitesgr03@gmail.com>
2024-12-25 19:06:51 +0800

    style: change double quote to single quote

--- src/components/layout/Dropdown.jsx
+++ src/components/layout/Dropdown.jsx
@@ -1,15 +1,15 @@
 // Packages
-import { useState } from "react";
-import PropTypes from "prop-types";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { useLocation } from "react-router-dom";
 
 // Styles
-import style from "../../styles/layout/Dropdown.module.css";
-import button from "../../styles/utils/button.module.css";
-import image from "../../styles/utils/image.module.css";
+import style from '../../styles/layout/Dropdown.module.css';
+import button from '../../styles/utils/button.module.css';
+import image from '../../styles/utils/image.module.css';
 
 // Components
-import Settings from "./Settings";
+import Settings from './Settings.jsx';
 
 // Utils
 export const Dropdown = ({
@@ -37,63 +37,56 @@ export const Dropdown = ({
 	const handleActiveSettings = () => setActiveSettings(true);
 	const handleCloseSettings = () => setActiveSettings(false);
 	return (
-		<div className={style.dropdown}>
-			{user?.name && (
-				<div className={style.profile}>
-					<div className={style.avatar}>
-						{user.name.charAt(0).toUpperCase()}
-					</div>
-					<span title={user.name}>{user.name}</span>
+				<div className={style.dropdown}>
+					{user?.name && (
+						<div className={style.profile}>
+							<div className={style.avatar}>
+								{user.name.charAt(0).toUpperCase()}
+							</div>
+							<span title={user.name}>{user.name}</span>
+						</div>
+					)}
+					<ul>
+						<li>
+							<button className={button.theme} onClick={onSwitchColorTheme}>
+								<span
+									className={`${image.icon} ${
+										darkTheme ? style.moon : style.sun
+									}`}
+								/>
+								{darkTheme ? 'Dark' : 'Light'} mode
+								<div>
+									<div />
+								</div>
+							</button>
+						</li>
+						{user && (
+							<li>
+								<button onClick={handleActiveSettings}>
+									<span className={`${image.icon} ${style.settings}`} />
+									Settings
+								</button>
+							</li>
+						)}
+						<li>
+							{user ? (
+								<button onClick={handleLogout}>
+									Logout
+								</button>
+							) : (
+									Login
+							)}
+						</li>
+					</ul>
+					{activeSettings && (
+						<Settings
+							user={user}
+							handleCloseSettings={handleCloseSettings}
+						/>
+					)}
 				</div>
 			)}
-			<ul>
-				<li>
-					<button
-						className={button.theme}
-						onClick={handleSwitchColorTheme}
-					>
-						<span
-							className={`${image.icon} ${
-								darkTheme ? style.moon : style.sun
-							}`}
-						/>
-						{darkTheme ? "Dark" : "Light"} mode
-						<div>
-							<div />
-						</div>
-					</button>
-				</li>
-				{user && (
-					<li>
-						<button onClick={handleActiveSettings}>
-							<span
-								className={`${image.icon} ${style.settings}`}
-							/>
-							Settings
-						</button>
-					</li>
-				)}
-				<li>
-					{user ? (
-						<button onClick={handleLogout}>
-							<span className={`${image.icon} ${style.logout}`} />
-							Logout
-						</button>
-					) : (
-						<button onClick={handleLogin}>
-							<span className={`${image.icon} ${style.login}`} />
-							Login
-						</button>
-					)}
-				</li>
-			</ul>
-			{activeSettings && (
-				<Settings
-					user={user}
-					handleCloseSettings={handleCloseSettings}
-				/>
-			)}
-		</div>
+		</>
 	);
 };
 
