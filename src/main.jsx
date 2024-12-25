d9cc1d0b3fb7d0fc2c5c476bbe4c283c434bcec5
Weiss Bai <whitesgr03@gmail.com>
2024-12-25 15:43:55 +0800

    style: change double quote to single quote

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import Router from "./router.jsx";
+  './router.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Router />
-	</React.StrictMode>
+	</React.StrictMode>,
 );
