2d26b63c90e4f5ea9f056b9bd85e6887d4a2028f
Weiss Bai <whitesgr03@gmail.com>
2024-12-25 15:54:55 +0800

    style: import declaration components

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './styles/index.css';
+
+import { App } from './components/App';
+import { NotFound } from './components/layout/NotFound';
+import { Home } from './components/Home';
+import { Login } from './components/pages/account/Login';
 
 import { Error } from './components/layout/Error';
 
