fbd94211916fd92771bb9471f7e7f12bdcfb0102
Weiss Bai <whitesgr03@gmail.com>
2024-12-25 18:27:27 +0800

    feat!: all functions do not need to receive token param and change export list to export declaration

--- src/utils/handlePost.js
+++ src/utils/handlePost.js
@@ -1,8 +1,8 @@
-import handleFetch from "./handleFetch";
+import { handleFetch } from './handleFetch';
 
 const url = `${import.meta.env.VITE_RESOURCE_URL}/blog/posts`;
 
-const getPosts = async ({ limit, signal }) => {
+export const getPosts = async ({ limit, signal }) => {
 	const options = {
 		method: "GET",
 		signal,
@@ -10,12 +10,10 @@ const getPosts = async ({ limit, signal }) => {
 	return await handleFetch(`${url}?limit=${limit}`, options);
 };
 
-const getPostDetail = async ({ postId, signal }) => {
+export const getPostDetail = async ({ postId, signal }) => {
 	const options = {
 		method: "GET",
 		signal,
 	};
 	return await handleFetch(`${url}/${postId}`, options);
 };
-
-export { getPosts, getPostDetail };
