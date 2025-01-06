f737415db7043db32610bde42b01f0dc01c6937d
Weiss Bai <whitesgr03@gmail.com>
2025-01-06 12:57:30 +0800

    feat!: delete handleGetPostDetail function
    The postDetail component receive posts prop from the Posts component

--- src/components/pages/Post/PostDetail.jsx
+++ src/components/pages/Post/PostDetail.jsx
@@ -1,7 +1,6 @@
 // Packages
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useParams, useOutletContext, Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { format } from 'date-fns';
 import { Editor } from '@tinymce/tinymce-react';
 
@@ -11,108 +10,65 @@ import imageStyles from '../../../styles/image.module.css';
 
 // Components
 import { Loading } from '../../utils/Loading';
-import { Error } from '../../utils/Error/Error';
 import { Comments } from '../Comment/Comments';
-
-// Utils
-import { getPostDetail } from '../../../utils/handlePost';
 
 export const PostDetail = () => {
 	const { postId } = useParams();
-	const { user } = useOutletContext();
-	const [post, setPost] = useState([]);
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(true);
+	const { posts, fetching } = useOutletContext();
 	const [loadContent, setLoadContent] = useState(true);
 
-	const createdAt = post?.createdAt && new Date(post.createdAt).getTime();
-	const lastModified =
-		post?.lastModified && new Date(post.lastModified).getTime();
+	const post = posts.find(post => post.id === postId);
 
-	useEffect(() => {
-		const controller = new AbortController();
-		const { signal } = controller;
-
-		const handleGetPostDetail = async () => {
-			const result = await getPostDetail({ postId, signal });
-
-			const handleResult = () => {
-				result.success ? setPost(result.data) : setError(result.message);
-				setLoading(false);
-			};
-
-			result && handleResult();
-		};
-
-		handleGetPostDetail();
-		return () => controller.abort();
-	}, [user, postId]);
 	return (
 		<>
-			{error || post?.content === '' ? (
-				<Error message={error} />
+			{fetching ? (
+				<Loading text={'Loading post...'} />
 			) : (
-				<>
-					<div className={`${loading || loadContent ? styles.loading : ''}`}>
-						<div id="post-detail" className={styles['post-detail']}>
-							<Link to="/posts" className={styles.link}>
-								<span className={`${styles['left-arrow']} ${imageStyles.icon}`} />
-								Back to list
-							</Link>
-
-							{post?.title && <h2 className={styles.title}>{post.title}</h2>}
-							<div className={styles['date-time']}>
-								<strong>{post?.author?.name}</strong>
-								{post?.lastModified && (
-									<em>
-										{`${
-											createdAt === lastModified ? 'Published' : 'Edited'
-										} in ${format(
-											createdAt === lastModified
-												? post.createdAt
-												: post.lastModified,
-											'MMMM d, y',
-										)}`}
-									</em>
-								)}
-
-								{post?.mainImageUrl && (
-									<div className={styles['image-wrap']}>
-										<div className={imageStyles.content}>
-											<img
-												src={post.mainImageUrl}
-												alt={`${post.title}'s main image`}
-											/>
-										</div>
+				<div className={loadContent ? styles.loading : ''}>
+					<div id="post-detail" className={styles['post-detail']}>
+						<Link to="/posts" className={styles.link}>
+							<span className={`${styles['left-arrow']} ${imageStyles.icon}`} />
+							Back to list
+						</Link>
+						{post?.title && <h2 className={styles.title}>{post.title}</h2>}
+						<div className={styles['date-time']}>
+							<strong>{post.author.username}</strong>
+							{post.updatedAt && (
+								<em>
+									{`${new Date(post.createdAt).getTime() !== new Date(post.updatedAt).getTime() ? 'Edited' : 'Published'} in ${format(post.updatedAt, 'MMMM d, y')}`}
+								</em>
+							)}
+							{post?.mainImageUrl && (
+								<div className={styles['image-wrap']}>
+									<div className={imageStyles.content}>
+										<img
+											src={post.mainImageUrl}
+											alt={`${post.title}'s main image`}
+										/>
 									</div>
-								)}
-							</div>
-							<Editor
-								tinymceScriptSrc="/tinymce/tinymce.min.js"
-								licenseKey="gpl"
-								id="content"
-								disabled={true}
-								value={post?.content}
-								onInit={() => {
-									setLoadContent(false);
-								}}
-								init={{
-									menubar: false,
-									toolbar: false,
-									inline: true,
-									plugins: 'codesample',
-								}}
-							/>
+								</div>
+							)}
 						</div>
-						<Comments postAuthorId={post?.author?._id} postId={postId} />
+						<Editor
+							tinymceScriptSrc="/tinymce/tinymce.min.js"
+							licenseKey="gpl"
+							id="content"
+							disabled={true}
+							value={post.content ?? ''}
+							onInit={() => {
+								setLoadContent(false);
+							}}
+							init={{
+								menubar: false,
+								toolbar: false,
+								inline: true,
+								plugins: 'codesample',
+							}}
+						/>
 					</div>
-					{(loading || loadContent) && <Loading />}
-				</>
+					<Comments postAuthorId={post?.author?._id} postId={postId} />
+				</div>
 			)}
 		</>
 	);
-};
-
-PostDetail.propTypes = {
-	post: PropTypes.object,
 };
