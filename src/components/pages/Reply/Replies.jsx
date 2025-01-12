ed5f6ee903b23ac8f22df91135f0622fb56b012f
Weiss Bai <whitesgr03@gmail.com>
2025-01-12 20:43:33 +0800

    feat: created a reply component to display all replies to a specified comment

---
+++ src/components/pages/Reply/Replies.jsx
@@ -0,0 +1,35 @@
+// Packages
+import PropTypes from 'prop-types';
+
+// Styles
+import styles from '../Comment/Comments.module.css';
+
+// Components
+import { ReplyDetail } from './ReplyDetail';
+
+export const Replies = ({ post, comment }) => {
+	const replies = comment?.replies ?? [];
+
+	return (
+		<div className={styles.replies}>
+			<div className={styles.content}>
+				<ul>
+					{replies.map(reply => (
+						<ReplyDetail
+							key={reply._id}
+							post={post}
+							comment={comment}
+							reply={reply}
+						/>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+Replies.propTypes = {
+	post: PropTypes.object,
+	comment: PropTypes.object,
+	onLoading: PropTypes.func,
+};
