ea5f7fb815f018216b1ec4fb4fee053a4b1b7884
Weiss Bai <whitesgr03@gmail.com>
2025-01-12 20:46:21 +0800

    feat: created a ReplyDelete component to delete a specified reply

---
+++ src/components/pages/Reply/ReplyDelete.jsx
@@ -0,0 +1,94 @@
+// Packages
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+// Styles
+import deleteModelStyles from '../../layout/Header/DeleteModel.module.css';
+import buttonStyles from '../../../styles/button.module.css';
+
+// Utils
+import { deleteReply } from '../../../utils/handleReply';
+
+// Components
+import { Loading } from '../../utils/Loading';
+
+export const ReplyDelete = ({
+	post,
+	commentId,
+	replyId,
+	onUpdatePost,
+	onAlert,
+	onActiveModal,
+}) => {
+	const [loading, setLoading] = useState(false);
+
+	const handleDeleteComment = async () => {
+		setLoading(true);
+
+		const result = await deleteReply({
+			replyId,
+		});
+
+		const handleSuccess = () => {
+			const newComments = post.comments.map(comment =>
+				comment._id === commentId
+					? {
+							...comment,
+							replies: comment.replies.map(reply =>
+								reply._id === replyId ? result.data : reply,
+							),
+						}
+					: comment,
+			);
+			onUpdatePost({ postId: post._id, newComments });
+			onAlert({ message: result.message });
+			onActiveModal({ component: null });
+		};
+
+		const handleFail = () => {
+			onAlert({
+				message: 'There are some errors occur, please try again later.',
+				error: true,
+			});
+			onActiveModal({ component: null });
+		};
+
+		result.success ? handleSuccess() : handleFail();
+
+		setLoading(false);
+	};
+	return (
+		<>
+			{loading && <Loading text={'Deleting...'} light={true} shadow={true} />}
+			<div className={deleteModelStyles.model}>
+				<span className={deleteModelStyles.title}>
+					Delete This Comment Forever
+				</span>
+				<span className={deleteModelStyles.content}>
+					Do you really want to delete?
+				</span>
+				<div className={deleteModelStyles['button-wrap']}>
+					<button
+						className={buttonStyles.cancel}
+						data-close-model
+						onClick={() => onActiveModal({ component: null })}
+					>
+						Cancel
+					</button>
+					<button className={buttonStyles.error} onClick={handleDeleteComment}>
+						Delete
+					</button>
+				</div>
+			</div>
+		</>
+	);
+};
+
+ReplyDelete.propTypes = {
+	post: PropTypes.object,
+	commentId: PropTypes.string,
+	replyId: PropTypes.string,
+	onUpdatePost: PropTypes.func,
+	onAlert: PropTypes.func,
+	onActiveModal: PropTypes.func,
+};
