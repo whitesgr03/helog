96ac20c7a2c9290ea7242c065d97cb0967cfb4f7
Weiss Bai <whitesgr03@gmail.com>
2025-01-12 17:57:56 +0800

    feat: create getReplies and createReply

--- src/utils/handleReply.js
+++ src/utils/handleReply.js
@@ -2,6 +2,17 @@ import { handleFetch } from './handleFetch';
 
 const url = `${import.meta.env.VITE_RESOURCE_URL}/blog`;
 
+export const getReplies = async ({ skip, commentId }) => {
+	const options = {
+		method: 'GET',
+		credentials: 'include',
+	};
+	return await handleFetch(
+		`${url}/comments/${commentId}/replies?skip=${skip}`,
+		options,
+	);
+};
+
 export const replyComment = async ({ commentId, data }) => {
 	const options = {
 		method: 'POST',
@@ -12,6 +23,18 @@ export const replyComment = async ({ commentId, data }) => {
 		body: JSON.stringify(data),
 	};
 	return await handleFetch(`${url}/comments/${commentId}/replies`, options);
+};
+
+export const createReply = async ({ replyId, data }) => {
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		credentials: 'include',
+		body: JSON.stringify(data),
+	};
+	return await handleFetch(`${url}/replies/${replyId}`, options);
 };
 
 export const updateReply = async ({ data, replyId }) => {
