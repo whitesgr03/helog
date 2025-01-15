5444897dc7c87f57cb928431075269091ae62f89
Weiss Bai <whitesgr03@gmail.com>
2025-01-15 19:33:04 +0800

    docs: recreated Account folder and login component

---
+++ src/components/Account/Login.jsx
@@ -0,0 +1,62 @@
+// Packages
+import { Navigate, useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
+
+// styles
+import styles from './Login.module.css';
+
+// Components
+import { Loading } from '../utils/Loading';
+
+// Assets
+import googleIcon from '../../../assets/google.png';
+import facebookIcon from '../../../assets/facebook.png';
+
+export const Login = () => {
+	const { user } = useOutletContext();
+
+	const [loading, setLoading] = useState(false);
+
+	const handleSocialLogin = async provider => {
+		setLoading(true);
+
+		window.location.assign(
+			`${import.meta.env.VITE_RESOURCE_URL}/account/login/${provider}`,
+		);
+
+		setLoading(false);
+	};
+
+	return (
+		<>
+			{user ? (
+				<Navigate to="/" replace={true} />
+			) : (
+				<div className={styles.account}>
+					<h3 className={styles.title}>User Sign in</h3>
+					<div className={styles.container}>
+						{loading && <Loading text={'Submitting...'} />}
+						<button
+							className={styles['federation-button']}
+							onClick={() => handleSocialLogin('google')}
+						>
+							<div className={styles.google}>
+								<img src={googleIcon} alt="Google icon" />
+							</div>
+							Sign in with Google
+						</button>
+						<button
+							className={styles['federation-button']}
+							onClick={() => handleSocialLogin('facebook')}
+						>
+							<div className={styles.facebook}>
+								<img src={facebookIcon} alt="Facebook icon" />
+							</div>
+							Sign in with Facebook
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
+};
