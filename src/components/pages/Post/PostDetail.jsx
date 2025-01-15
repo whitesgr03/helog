b454370837e30d56b6653e7e3f128e239404f653
Weiss Bai <whitesgr03@gmail.com>
2025-01-15 14:14:11 +0800

    feat!: changed the conditions to render Loading component

--- src/components/pages/Post/PostDetail.jsx
+++ src/components/pages/Post/PostDetail.jsx
@@ -23,7 +23,7 @@ import { getPostDetail } from '../../../utils/handlePost';
 
 export const PostDetail = () => {
 	const { postId } = useParams();
-	const { posts, fetching, onUpdatePost } = useOutletContext();
+	const { posts, onUpdatePost } = useOutletContext();
 	const [loadContent, setLoadContent] = useState(true);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
@@ -57,52 +57,56 @@ export const PostDetail = () => {
 		<>
 			{error ? (
 				<Navigate to="/error" state={{ error, previousPath }} />
-      ) : (
-          {(fetching || loading) && <Loading text={'Loading post...'} />}
-				<div className={loadContent ? styles.loading : ''}>
-					<div id="post-detail" className={styles['post-detail']}>
-						<Link to={-1} className={styles.link}>
-							<span className={`${styles['left-arrow']} ${imageStyles.icon}`} />
-							Back to previous page
-						</Link>
-						{post?.title && <h2 className={styles.title}>{post.title}</h2>}
-						<div className={styles['date-time']}>
-							<strong>{post.author.username}</strong>
-							<em>{`Published in ${format(post.createdAt, 'MMMM d, y')}`}</em>
-							{new Date(post.createdAt).getDate() !==
-								new Date(post.updatedAt).getDate() && (
-								<em>{`Edited in ${format(post.updatedAt, 'MMMM d, y')}`}</em>
-							)}
-							{post?.mainImageUrl && (
-								<div className={styles['image-wrap']}>
-									<div className={imageStyles.content}>
-										<img
-											src={post.mainImageUrl}
-											alt={`${post.title}'s main image`}
-										/>
+			) : (
+				<>
+					{(loading || loadContent) && <Loading text={'Loading post...'} />}
+					<div className={loadContent ? styles.loading : ''}>
+						<div id="post-detail" className={styles['post-detail']}>
+							<Link to={-1} className={styles.link}>
+								<span
+									className={`${styles['left-arrow']} ${imageStyles.icon}`}
+								/>
+								Back to previous page
+							</Link>
+							{post?.title && <h2 className={styles.title}>{post.title}</h2>}
+							<div className={styles['date-time']}>
+								<strong>{post.author.username}</strong>
+								<em>{`Published in ${format(post.createdAt, 'MMMM d, y')}`}</em>
+								{new Date(post.createdAt).getDate() !==
+									new Date(post.updatedAt).getDate() && (
+									<em>{`Edited in ${format(post.updatedAt, 'MMMM d, y')}`}</em>
+								)}
+								{post?.mainImageUrl && (
+									<div className={styles['image-wrap']}>
+										<div className={imageStyles.content}>
+											<img
+												src={post.mainImageUrl}
+												alt={`${post.title}'s main image`}
+											/>
+										</div>
 									</div>
-								</div>
-							)}
+								)}
+							</div>
+							<Editor
+								tinymceScriptSrc="/tinymce/tinymce.min.js"
+								licenseKey="gpl"
+								id="content"
+								disabled={true}
+								value={post.content ?? ''}
+								onInit={() => {
+									setLoadContent(false);
+								}}
+								init={{
+									menubar: false,
+									toolbar: false,
+									inline: true,
+									plugins: 'codesample',
+								}}
+							/>
 						</div>
-						<Editor
-							tinymceScriptSrc="/tinymce/tinymce.min.js"
-							licenseKey="gpl"
-							id="content"
-							disabled={true}
-							value={post.content ?? ''}
-							onInit={() => {
-								setLoadContent(false);
-							}}
-							init={{
-								menubar: false,
-								toolbar: false,
-								inline: true,
-								plugins: 'codesample',
-							}}
-						/>
+						<Comments post={post} />
 					</div>
-					<Comments post={post} />
-				</div>
+				</>
 			)}
 		</>
 	);
