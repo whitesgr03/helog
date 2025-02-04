92814cbb3e6ccc8c716d2ee711f13530366a946a
Weiss Bai <whitesgr03@gmail.com>
2025-02-04 21:41:26 +0800

    refactor: changed Login test file

--- src/__test__/pages/Account/Login.test.jsx
+++ src/__test__/pages/Account/Login.test.jsx
@@ -1,224 +1,164 @@
 import { vi, describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 
-import Login from '../components/Login';
+import { RouterProvider, createMemoryRouter, Outlet } from 'react-router-dom';
 
-import handleFetch from '../../../utils/handleFetch';
+import { Login } from '../../../components/pages/Account/Login';
+import { Loading } from '../../../components/utils/Loading';
 
-vi.mock('../components/Error.jsx', () => ({
-	default: () => <div>Error component</div>,
-}));
-
-vi.mock('../utils/handleFetch');
-
-/* eslint-disable react/prop-types */
-const Providers = ({ children, ...props }) => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<Outlet context={{ ...props }} />}>
-					<Route index element={children} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
-};
+vi.mock('../../../components/utils/Loading');
 
 describe('Login component', () => {
-	it("should render errors for each input if 'handleValidFields' fails on submission.", async () => {
-		const user = userEvent.setup();
-		const mockOutContext = {
-			setToken: vi.fn(),
+	it('should navigate to the "/" path if the user prop is provided', async () => {
+		const mockContext = {
+			user: true,
 		};
 
-		render(
-			<Providers {...mockOutContext}>
-				<Login />
-			</Providers>,
-		);
-
-		const submitBtn = screen.getByRole('button', { name: 'Login' });
-
-		let defaultErrorMessages = screen.getAllByText('Message placeholder');
-
-		expect(defaultErrorMessages.length).toEqual(2);
-
-		await user.click(submitBtn);
-
-		defaultErrorMessages = screen.queryAllByText('Message placeholder');
-
-		expect(defaultErrorMessages.length).toEqual(0);
-	});
-	it('should continue valid fields when entering again after submission failed.', async () => {
-		const user = userEvent.setup();
-		const mockOutContext = {
-			setToken: vi.fn(),
-		};
-
-		render(
-			<Providers {...mockOutContext}>
-				<Login />
-			</Providers>,
-		);
-
-		const submitBtn = screen.getByRole('button', { name: 'Login' });
-		const emailInput = screen.getByLabelText('Email');
-		const passwordInput = screen.getByLabelText('Password');
-
-		await user.click(submitBtn);
-
-		let defaultErrorMessages = screen.queryAllByText('Message placeholder');
-
-		expect(defaultErrorMessages.length).toEqual(0);
-
-		await user.type(emailInput, 'example@gmail.com');
-		await user.type(passwordInput, '1!Qwerty');
-
-		defaultErrorMessages = await screen.findAllByText('Message placeholder');
-
-		expect(defaultErrorMessages.length).toEqual(2);
-	});
-	it("should set token if the 'handleLogin' fetch result is successfully", async () => {
-		const user = userEvent.setup();
-		const mockOutletContext = {
-			setToken: vi.fn(),
-		};
-
-		const mockResult = {
-			success: true,
-			data: {
-				token: 'exampleToken',
-			},
-		};
-
-		handleFetch.mockReturnValueOnce(mockResult);
-
-		render(
-			<Providers {...mockOutletContext}>
-				<Login />
-			</Providers>,
-		);
-
-		const emailInput = screen.getByLabelText('Email');
-		const passwordInput = screen.getByLabelText('Password');
-		const submitBtn = screen.getByRole('button', { name: 'Login' });
-
-		await user.type(emailInput, 'example@gmail.com');
-		await user.type(passwordInput, '1!Qwerty');
-
-		await user.click(submitBtn);
-
-		expect(handleFetch).toBeCalledTimes(1);
-		expect(mockOutletContext.setToken).toBeCalledTimes(1);
-	});
-	it("should render errors for each input if 'handleLogin' fetch result is fails.", async () => {
-		const user = userEvent.setup();
-		const mockOutletContext = {
-			setToken: vi.fn(),
-		};
-
-		const mockResult = {
-			success: false,
-			errors: [
+		const router = createMemoryRouter(
+			[
 				{
-					field: 'email',
-					message: 'error',
-				},
-				{
-					field: 'password',
-					message: 'error',
+					path: '/',
+					element: <Outlet context={{ ...mockContext }} />,
+					children: [
+						{
+							index: true,
+							element: <div>Home component</div>,
+						},
+						{
+							path: 'login',
+							element: <Login />,
+						},
+					],
 				},
 			],
-		};
-
-		handleFetch.mockReturnValueOnce(mockResult);
-
-		render(
-			<Providers {...mockOutletContext}>
-				<Login />
-			</Providers>,
+			{
+				initialEntries: ['/login'],
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
 		);
 
-		const emailInput = screen.getByLabelText('Email');
-		const passwordInput = screen.getByLabelText('Password');
-		const submitBtn = screen.getByRole('button', { name: 'Login' });
-
-		await user.type(emailInput, 'example@gmail.com');
-		await user.type(passwordInput, '1!Qwerty');
-
-		let defaultErrorMessages = screen.getAllByText('Message placeholder');
-
-		expect(defaultErrorMessages.length).toEqual(2);
-
-		await user.click(submitBtn);
-
-		defaultErrorMessages = screen.queryAllByText('Message placeholder');
-
-		expect(defaultErrorMessages.length).toEqual(0);
-		expect(handleFetch).toBeCalledTimes(1);
-	});
-	it("should render the Error component if the 'handleLogin' fetch result is fails.", async () => {
-		const user = userEvent.setup();
-		const mockOutletContext = {
-			setToken: vi.fn(),
-		};
-
-		const mockResult = {
-			success: false,
-			message: 'error',
-		};
-
-		handleFetch.mockReturnValueOnce(mockResult);
-
 		render(
-			<Providers {...mockOutletContext}>
-				<Login />
-			</Providers>,
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
 		);
 
-		const emailInput = screen.getByLabelText('Email');
-		const passwordInput = screen.getByLabelText('Password');
-		const submitBtn = screen.getByRole('button', { name: 'Login' });
+		const homeComponent = screen.getByText('Home component');
 
-		await user.type(emailInput, 'example@gmail.com');
-		await user.type(passwordInput, '1!Qwerty');
-
-		await user.click(submitBtn);
-
-		const element = screen.getByText('Error component');
-
-		expect(handleFetch).toBeCalledTimes(1);
-		expect(element).toBeInTheDocument();
+		expect(homeComponent).toBeInTheDocument();
 	});
-	it("should render the Error component if an error is captured while performing 'handleLogin'.", async () => {
+	it('should login with google account if the "Sign in with Google" button is clicked', async () => {
 		const user = userEvent.setup();
-		const mockOutletContext = {
-			setToken: vi.fn(),
+		const mockContext = {
+			user: false,
 		};
 
-		handleFetch.mockImplementationOnce(() => {
-			throw new Error('Fetch Error');
+		const mockFn = vi.fn();
+
+		Object.defineProperty(window, 'location', {
+			value: {
+				assign: mockFn,
+			},
 		});
-		render(
-			<Providers {...mockOutletContext}>
-				<Login />
-			</Providers>,
+
+		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Outlet context={{ ...mockContext }} />,
+					children: [
+						{
+							index: true,
+							element: <Login />,
+						},
+					],
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
 		);
 
-		const emailInput = screen.getByLabelText('Email');
-		const passwordInput = screen.getByLabelText('Password');
-		const submitBtn = screen.getByRole('button', { name: 'Login' });
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
 
-		await user.type(emailInput, 'example@gmail.com');
-		await user.type(passwordInput, '1!Qwerty');
+		const googleButton = screen.getByText('Sign in with Google');
 
-		await user.click(submitBtn);
+		user.click(googleButton);
 
-		const element = screen.getByText('Error component');
+		await screen.findByText('Loading component');
 
-		expect(handleFetch).toBeCalledTimes(1);
-		expect(element).toBeInTheDocument();
+		expect(mockFn).toBeCalledTimes(1);
+		expect(mockFn.mock.calls[0][0]).toMatch(/google/);
+	});
+	it('should login with facebook account if "Sign in with Facebook" button is clicked', async () => {
+		const user = userEvent.setup();
+		const mockContext = {
+			user: false,
+		};
+
+		const mockFn = vi.fn();
+
+		Object.defineProperty(window, 'location', {
+			value: {
+				assign: mockFn,
+			},
+		});
+
+		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Outlet context={{ ...mockContext }} />,
+					children: [
+						{
+							index: true,
+							element: <Login />,
+						},
+					],
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const googleButton = screen.getByText('Sign in with Facebook');
+
+		user.click(googleButton);
+
+		await screen.findByText('Loading component');
+
+		expect(mockFn).toBeCalledTimes(1);
+		expect(mockFn.mock.calls[0][0]).toMatch(/facebook/);
 	});
 });
