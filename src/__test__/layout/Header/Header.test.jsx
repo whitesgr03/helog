5e113de06dd429b84c05db183fe4e3ea0f1a3f86
Weiss Bai <whitesgr03@gmail.com>
2025-02-04 15:36:08 +0800

    refactor: changed Header test file

--- src/__test__/layout/Header/Header.test.jsx
+++ src/__test__/layout/Header/Header.test.jsx
@@ -1,88 +1,239 @@
 import { vi, describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter } from 'react-router-dom';
+
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 import { Header } from '../../../components/layout/Header/Header';
+import { Dropdown } from '../../../components/layout/Header/Dropdown';
 
-vi.mock('../components/Dropdown.jsx', () => ({
-	default: () => <div>Dropdown component</div>,
-}));
+vi.mock('../../../components/layout/Header/Dropdown');
 
 describe('Header component', () => {
-	it('should render link of write, if the user of isAdmin is true', () => {
-		const mockUser = {
-			isAdmin: true,
+	it('should navigate to the "../" path, if the HeLog link is clicked', async () => {
+		const user = userEvent.setup();
+		const mockProps = {};
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/header',
+					element: <Header {...mockProps} />,
+				},
+				{
+					path: '/',
+					element: <div>Home component</div>,
+				},
+			],
+			{
+				initialEntries: ['/header'],
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const link = screen.getByRole('link', { name: 'HeLog' });
+
+		await user.click(link);
+
+		const homeComponent = screen.getByText('Home component');
+
+		expect(homeComponent).toBeInTheDocument();
+	});
+	it('should render the post editor link, if the username of user prop is provided', () => {
+		const mockProps = {
+			user: {
+				username: 'example',
+			},
 		};
 
-		render(<Header user={mockUser} />, { wrapper: BrowserRouter });
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Header {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
 
 		const element = screen.getByRole('link', { name: 'Write' });
 
 		expect(element).toBeInTheDocument();
 	});
-	it("should render the dark color theme, if the 'darkTheme' prop is true", () => {
-		const mockDarkTheme = true;
-		render(<Header darkTheme={mockDarkTheme} />, {
-			wrapper: BrowserRouter,
-		});
+	it("should render the dark mode icon and text, if the 'darkTheme' prop is provided", () => {
+		const mockProps = {
+			darkTheme: true,
+		};
 
-		const element = screen.getByRole('button', { name: /Dark/i });
-
-		expect(element).toBeInTheDocument();
-	});
-	it('should change the color theme, if the color them button is clicked', async () => {
-		const user = userEvent.setup();
-		const mockDarkTheme = true;
-		const mockHandleSwitchColorTheme = vi.fn();
-		render(
-			<Header
-				darkTheme={mockDarkTheme}
-				handleSwitchColorTheme={mockHandleSwitchColorTheme}
-			/>,
-			{ wrapper: BrowserRouter },
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Header {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
 		);
 
-		const element = screen.getByRole('button', { name: /Dark/i });
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
 
-		await user.click(element);
+		const button = screen.getByRole('button', { name: 'Dark mode' });
+		const icon = screen.getByTestId('theme-icon');
 
-		expect(mockHandleSwitchColorTheme).toBeCalledTimes(1);
+		expect(button).toBeInTheDocument();
+		expect(icon).toHaveClass(/moon/);
 	});
-	it('should render Dropdown component and transparent bgc, if the account btn is clicked', async () => {
+	it('should switch color theme, if the color theme button is clicked', async () => {
 		const user = userEvent.setup();
+		const mockProps = {
+			onColorTheme: vi.fn(),
+		};
 
-		render(<Header />, { wrapper: BrowserRouter });
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Header {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
 
-		const accountBtn = screen.getByRole('button', { name: 'Account' });
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
 
-		await user.click(accountBtn);
+		const button = screen.getByRole('button', { name: 'Light mode' });
+		const icon = screen.getByTestId('theme-icon');
 
-		const component = screen.getByText('Dropdown component');
+		expect(icon).toHaveClass(/sun/);
+
+		await user.click(button);
+
+		expect(mockProps.onColorTheme).toBeCalledTimes(1);
+	});
+	it('should render Dropdown component and transparent bgc, if the account button is clicked', async () => {
+		const user = userEvent.setup();
+		const mockProps = {};
+
+		Dropdown.mockImplementationOnce(() => <div>Dropdown component</div>);
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Header {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const accountButton = screen.getByRole('button', { name: 'Account' });
+
+		await user.click(accountButton);
+
+		const dropdownComponent = screen.getByText('Dropdown component');
 		const element = screen.getByTestId('transparentBgc');
 
-		expect(component).toBeInTheDocument();
+		expect(dropdownComponent).toBeInTheDocument();
 		expect(element).toBeInTheDocument();
 	});
 	it('should close Dropdown component and transparent bgc, if the transparent bgc is clicked', async () => {
 		const user = userEvent.setup();
+		const mockProps = {};
 
-		render(<Header />, { wrapper: BrowserRouter });
+		Dropdown.mockImplementationOnce(() => <div>Dropdown component</div>);
 
-		const accountBtn = screen.getByRole('button', { name: 'Account' });
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Header {...mockProps} />,
+				},
+			],
+			{
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
 
-		await user.click(accountBtn);
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
 
-		let element = screen.getByTestId('transparentBgc');
+		const accountButton = screen.getByRole('button', { name: 'Account' });
+
+		await user.click(accountButton);
+
+		const dropdownComponent = screen.getByText('Dropdown component');
+		const element = screen.getByTestId('transparentBgc');
 
 		await user.click(element);
 
-		const component = screen.queryByText('dropdown');
-		element = screen.queryByTestId('transparentBgc');
-
-		expect(component).not.toBeInTheDocument();
+		expect(dropdownComponent).not.toBeInTheDocument();
 		expect(element).not.toBeInTheDocument();
 	});
 });
-
-// 繼續從 header 往下驗證
