617e8c0b68ade5078cca4e375fc4b84f7c46f981
Weiss Bai <whitesgr03@gmail.com>
2025-02-04 17:30:08 +0800

    refactor: changed Loading test file

--- src/__test__/utils/Loading.test.jsx
+++ src/__test__/utils/Loading.test.jsx
@@ -1,14 +1,54 @@
-import { describe, it, expect } from "vitest";
-import { render } from "@testing-library/react";
+import { expect, describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
-import Loading from "../components/Loading";
+import { Loading } from '../../components/utils/Loading';
 
-describe("Loading component", () => {
-	it("should render correctly", () => {
-		const { asFragment } = render(<Loading />);
+describe('Loading component', () => {
+	it('should render the text if the text prop is provided', () => {
+		const mockProp = {
+			text: 'loading',
+		};
 
-		const actual = asFragment();
+		render(<Loading {...mockProp} />);
 
-		expect(actual).toMatchSnapshot();
+		const element = screen.getByText(mockProp.text);
+
+		expect(element).toBeInTheDocument();
+	});
+	it('should render the dark class if the dark prop is provided', () => {
+		const mockProp = {
+			dark: true,
+			text: 'loading',
+		};
+
+		render(<Loading {...mockProp} />);
+
+		const element = screen.getByText(mockProp.text);
+
+		expect(element).toHaveClass(/dark/);
+	});
+	it('should render the light class if the light prop is provided', () => {
+		const mockProp = {
+			light: true,
+			text: 'loading',
+		};
+
+		render(<Loading {...mockProp} />);
+
+		const element = screen.getByText(mockProp.text);
+
+		expect(element).toHaveClass(/light/);
+	});
+	it('should render the shadow class if the shadow prop is provided', () => {
+		const mockProp = {
+			shadow: true,
+			text: 'loading',
+		};
+
+		render(<Loading {...mockProp} />);
+
+		const element = screen.getByText(mockProp.text);
+
+		expect(element).toHaveClass(/shadow/);
 	});
 });
