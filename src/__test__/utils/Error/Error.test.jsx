fc05658843265622e621b85c4cc24fe52a762458
Weiss Bai <whitesgr03@gmail.com>
2025-02-04 18:45:54 +0800

    refactor: changed Error test file

--- src/__test__/utils/Error/Error.test.jsx
+++ src/__test__/utils/Error/Error.test.jsx
@@ -1,23 +1,119 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { expect, describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter, Navigate } from 'react-router-dom';
+import { Error } from '../../../components/utils/Error/Error';
 
-import Error from "../components/Error";
+describe('Loading component', () => {
+	it('should render the default error message and default link if the "customMessage" state is not provided', () => {
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Navigate to={'/error'} />,
+				},
+				{
+					path: '/error',
+					element: <Error />,
+				},
+			],
+			{
+				initialEntries: ['/'],
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
 
-describe("Error component", () => {
-	it("should render message from prop", () => {
-		const mockMessage = "error message";
-		render(<Error message={mockMessage} />);
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
 
-		const element = screen.getByText(mockMessage);
+		const element = screen.getByText(
+			'Please come back later, or if you have any questions, contact us.',
+		);
+
+		const link = screen.getByRole('link', { name: 'Back to Home Page' });
+
+		expect(element).toBeInTheDocument();
+		expect(link).toBeInTheDocument();
+	});
+	it('should render the custom error message if the "customMessage" state is provided', () => {
+		const mockState = {
+			error: 'custom message.',
+			customMessage: true,
+		};
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Navigate to={'/error'} state={{ ...mockState }} />,
+				},
+				{
+					path: '/error',
+					element: <Error />,
+				},
+			],
+			{
+				initialEntries: ['/'],
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const element = screen.getByText('custom message.');
 
 		expect(element).toBeInTheDocument();
 	});
-	it("should render default message if type of message prop is not string", () => {
-		render(<Error message={{}} />);
+	it('should render the "Go Back" link if the "previousPath" state is provided', () => {
+		const mockState = {
+			previousPath: '/',
+		};
 
-		const element = screen.getByText(
-			"Please come back later or if you have any questions, please contact us."
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Navigate to={'/error'} state={{ ...mockState }} />,
+				},
+				{
+					path: '/error',
+					element: <Error />,
+				},
+			],
+			{
+				initialEntries: ['/'],
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
 		);
+
+		render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
+
+		const element = screen.getByRole('link', { name: 'Go Back' });
 
 		expect(element).toBeInTheDocument();
 	});
