0972faef1fc63f2deb170e4a098550fecd19f3ab
Weiss Bai <whitesgr03@gmail.com>
2025-02-05 17:17:53 +0800

    feat: added the dataset variables for testing

--- src/components/pages/Reply/ReplyUpdate.jsx
+++ src/components/pages/Reply/ReplyUpdate.jsx
@@ -172,7 +172,10 @@ export const ReplyUpdate = ({ post, commentId, reply, onCloseCommentBox }) => {
 				onSubmit={e => !loading && handleSubmit(e)}
 			>
 				<div className={formStyles['label-wrap']}>
-					<label className={`${inputErrors.content ? formStyles.error : ''}`}>
+					<label
+						className={`${inputErrors.content ? formStyles.error : ''}`}
+						data-testid="label"
+					>
 						<textarea
 							name="content"
 							placeholder="write a comment..."
@@ -184,7 +187,7 @@ export const ReplyUpdate = ({ post, commentId, reply, onCloseCommentBox }) => {
 						/>
 					</label>
 					{!isEmpty(inputErrors) && (
-						<div>
+						<div data-testid="error-message">
 							<span className={`${imageStyles.icon} ${formStyles.alert}`} />
 							<span className={formStyles.placeholder}>
 								{inputErrors.content}
