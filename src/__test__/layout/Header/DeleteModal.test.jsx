77e044af7b7464c68b936c18e63ff5dc7bc6f51c
Weiss Bai <whitesgr03@gmail.com>
2025-02-05 15:35:09 +0800

    feat!: removed waitFor module

--- src/__test__/layout/Header/DeleteModal.test.jsx
+++ src/__test__/layout/Header/DeleteModal.test.jsx
@@ -1,5 +1,5 @@
 import { vi, describe, it, expect } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
@@ -60,13 +60,11 @@ describe('DeleteModal component', () => {
 
 		const loadingComponent = await screen.findByText('Loading component');
 
-		await waitFor(async () => {
-			const errorComponent = await screen.findByText('Error component');
+		const errorComponent = await screen.findByText('Error component');
 
-			expect(deleteUser).toBeCalledTimes(1);
-			expect(errorComponent).toBeInTheDocument();
-			expect(loadingComponent).not.toBeInTheDocument();
-		});
+		expect(deleteUser).toBeCalledTimes(1);
+		expect(errorComponent).toBeInTheDocument();
+		expect(loadingComponent).not.toBeInTheDocument();
 	});
 	it('should close modal if cancel button is clicked', async () => {
 		const user = userEvent.setup();
