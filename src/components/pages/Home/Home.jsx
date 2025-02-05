5d1ccce34b4283b2a56c6c001d71833ad13cfa20
Weiss Bai <whitesgr03@gmail.com>
2025-02-05 09:19:00 +0800

    feat!: changed to use string templates to render single quotes

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -27,7 +27,7 @@ export const Home = () => {
 						The personal blog is an ongoing online diary or commentary written
 						by an individual, rather than a corporation or organization. While
 						the vast majority of personal blogs attract very few readers, other
-						than the blogger&apos;s immediate family and friends, a small number
+						than the blogger{`'`}s immediate family and friends, a small number
 						of personal blogs have become popular, to the point that they have
 						attracted lucrative advertising sponsorship. A tiny number of
 						personal bloggers have become famous, both in the online community
