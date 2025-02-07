8cbee904d30d569bb8e752220a015fe86f19bc0c
Weiss Bai <whitesgr03@gmail.com>
2025-02-08 01:01:13 +0800

    chore: added the include and exclude options

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,11 @@ export default defineConfig({
 		globals: true,
 		environment: 'jsdom',
 		setupFiles: './src/__test__/setup.js',
+		include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+		exclude: ['src/**/delete_*.{test,spec}.?(c|m)[jt]s?(x)'],
+		coverage: {
+			include: ['src/**'],
+			exclude: ['src/**/delete_**'],
+		},
 	},
 });
