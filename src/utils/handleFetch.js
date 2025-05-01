b1fd2ed51a62e23b7dffd34b4b490c8e9c57f660
Weiss Bai <whitesgr03@gmail.com>
2025-05-01 20:39:55 +0800

    refactor: handleFetch function

--- src/utils/handleFetch.js
+++ src/utils/handleFetch.js
@@ -1,15 +1,9 @@
-export const handleFetch = async (url, option) => {
-	try {
-		const response = await fetch(url, { ...option });
-		const result = await response.json();
-		response.status !== 200 && (result.status = response.status);
-		return result;
-	} catch (err) {
-		return (
-			!option.signal.aborted && {
-				success: false,
-				message: err,
-			}
-		);
-	}
+export const handleFetch = async (url, options) => {
+	const response = await fetch(url, options).catch(error => {
+		throw new Error(error);
+	});
+
+	if (!response.ok) throw new Error(response.status);
+
+	return response.json();
 };
