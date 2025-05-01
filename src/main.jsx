1f720f5e30c800d3a22221ebf259834a6af9fbfa
Weiss Bai <whitesgr03@gmail.com>
2025-05-01 20:38:58 +0800

    feat: added QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { Router } from './Router.jsx';
+import { queryClient } from './utils/queryOptions.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Router />
+		<QueryClientProvider client={queryClient}>
+			<Router />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</React.StrictMode>,
 );
