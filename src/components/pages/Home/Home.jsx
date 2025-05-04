0c63f07046d863629f74b893c768484f80e61b5a
Weiss Bai <whitesgr03@gmail.com>
2025-05-04 18:23:02 +0800

    feat: render a refetch posts button instead of the Navigate component when the error occurs

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import { Loading } from '../../utils/Loading';
 // Styles
 import styles from './Home.module.css';
 import imageStyles from '../../../styles/image.module.css';
+import buttonStyles from '../../../styles/button.module.css';
 
 // Components
 import { Posts } from '../Post/Posts';
@@ -34,57 +35,53 @@ export const Home = () => {
 		},
 	});
 
-	const { pathname: previousPath } = useLocation();
-
 	const posts = data?.pages[0].data.posts.slice(0, 4);
 
 	return (
 		<div className={styles.home}>
-			{isError ? (
-				<Navigate to="/error" state={{ previousPath }} />
-			) : (
-				<>
-					<div className={styles.container}>
-						<div className={styles.wrap}>
-							<div className={imageStyles.content}>
-								<img src={url} />
-							</div>
-						</div>
-						<div className={styles.content}>
-							<h2 className={styles.title}>
-								Welcome to HeLog: A Daily Stories For Reader
-							</h2>
-							<p>
-								Ever felt like your daily experiences are worth sharing? Those
-								little moments that make you smile, the challenges that shaped
-								you, or the random thoughts that keep you up at night? Well,
-								you're not alone, and that's exactly why HeLog exists. Think of
-								HeLog as your digital diary that connects with others. It's
-								where your everyday experiences become part of a larger tapestry
-								of human stories. No filters, no pretense - just real people
-								sharing real moments. Let us make your story part of the world's
-								story.
-							</p>
-							<Link to={'../posts'} className={styles.link}>
-								All Posts
-								<span
-									className={`${imageStyles.icon} ${styles.doubleArrowRight}`}
-								/>
-							</Link>
-						</div>
+			<div className={styles.container}>
+				<div className={styles.wrap}>
+					<div className={imageStyles.content}>
+						<img src={url} />
 					</div>
-					<div className={styles['latest-posts']}>
-						{isPending ? (
-							<Loading text={'Loading Posts...'} />
-						) : (
-							<>
-								<h2>Latest Posts</h2>
-								<Posts posts={posts} />
-							</>
-						)}
-					</div>
-				</>
-			)}
+				</div>
+				<div className={styles.content}>
+					<h2 className={styles.title}>
+						Welcome to HeLog: A Daily Stories For Reader
+					</h2>
+					<p>
+						Ever felt like your daily experiences are worth sharing? Those
+						little moments that make you smile, the challenges that shaped you,
+						or the random thoughts that keep you up at night? Well, you're not
+						alone, and that's exactly why HeLog exists. Think of HeLog as your
+						digital diary that connects with others. It's where your everyday
+						experiences become part of a larger tapestry of human stories. No
+						filters, no pretense - just real people sharing real moments. Let us
+						make your story part of the world's story.
+					</p>
+					<Link to={'../posts'} className={styles.link}>
+						All Posts
+						<span
+							className={`${imageStyles.icon} ${styles.doubleArrowRight}`}
+						/>
+					</Link>
+				</div>
+			</div>
+			<div className={styles['latest-posts']}>
+				<h2>Latest Posts</h2>
+				{isError && !data?.pages.length ? (
+					<button
+						className={`${buttonStyles.content} ${buttonStyles.more}`}
+						onClick={() => refetch()}
+					>
+						Click here to load posts
+					</button>
+				) : isPending ? (
+					<Loading text={'Loading posts ...'} />
+				) : (
+					<Posts posts={posts} />
+				)}
+			</div>
 		</div>
 	);
 };
