99ed9868b351b18202fd8f8f8be30151189720d7
Weiss Bai <whitesgr03@gmail.com>
2025-05-06 00:55:53 +0800

    feat: added the useQuery method to fetch the post detail

--- src/components/pages/Post/PostDetail.jsx
+++ src/components/pages/Post/PostDetail.jsx
@@ -1,14 +1,9 @@
 // Packages
 import { useEffect, useState, useRef } from 'react';
-import {
-	useParams,
-	useOutletContext,
-	Link,
-	Navigate,
-	useLocation,
-} from 'react-router-dom';
+import { useParams, Link, Navigate, useLocation } from 'react-router-dom';
 import { format } from 'date-fns';
 import { Editor } from '@tinymce/tinymce-react';
+import { useQuery } from '@tanstack/react-query';
 
 // Styles
 import styles from './PostDetail.module.css';
@@ -19,29 +14,25 @@ import { Loading } from '../../utils/Loading';
 import { Comments } from '../Comment/Comments';
 
 // Utils
-import { getPostDetail } from '../../../utils/handlePost';
+import { queryPostDetailOption } from '../../../utils/queryOptions';
 
 export const PostDetail = () => {
 	const { postId } = useParams();
-	const { posts, onUpdatePost } = useOutletContext();
 	const [contentEditorLoad, setContentEditorLoad] = useState(true);
-	const [loading, setLoading] = useState(true);
 	const [checking, setChecking] = useState(true);
-	const [error, setError] = useState(null);
 	const [errorImage, setErrorImage] = useState(null);
+
 	const imageContentRef = useRef(null);
 	const contentRef = useRef(null);
-	const [currentPost, setCurrentPost] = useState(null);
-
-	const appComponentPost = posts.find(post => post._id === postId);
-
-	const post = appComponentPost ?? currentPost;
 
 	const { pathname: previousPath } = useLocation();
 
-	const handleUpdateCurrentPost = ({ newPost, newComments: comments }) => {
-		setCurrentPost(newPost ? newPost : { ...post, comments });
-	};
+	const {
+		isPending,
+		isError,
+		data: post,
+		error,
+	} = useQuery(queryPostDetailOption(postId));
 
 	const handleError = () => {
 		const content = imageContentRef.current;
@@ -53,31 +44,6 @@ export const PostDetail = () => {
 
 	const handleLoad = e =>
 		(e.target.width <= 0 || e.target.height <= 0) && handleError();
-
-	useEffect(() => {
-		const controller = new AbortController();
-		const { signal } = controller;
-
-		const handleGetPostDetail = async () => {
-			const result = await getPostDetail({ postId, signal });
-			const handleResult = () => {
-				result.success
-					? appComponentPost
-						? onUpdatePost({ postId, newPost: result.data })
-						: setCurrentPost(result.data)
-					: setError(result);
-				setLoading(false);
-			};
-
-			result && handleResult();
-		};
-
-		appComponentPost?.content || currentPost
-			? setLoading(false)
-			: handleGetPostDetail();
-
-		return () => controller.abort();
-	}, [currentPost, appComponentPost, postId, onUpdatePost]);
 
 	useEffect(() => {
 		const handleCheckContentImages = async () => {
@@ -115,77 +81,82 @@ export const PostDetail = () => {
 			setChecking(false);
 		};
 
-		!loading && !contentEditorLoad && handleCheckContentImages();
-	}, [loading, contentEditorLoad]);
+		!isPending && !contentEditorLoad && handleCheckContentImages();
+	}, [isPending, contentEditorLoad]);
 
 	return (
 		<>
 			<div id="post-detail" className={styles['post-detail']}>
-				{error ? (
-					error.status === 404 ? (
+				{isError ? (
+					error.cause.status === 404 ? (
 						<Navigate to="/error/404" />
 					) : (
-						<Navigate to="/error" state={{ error, previousPath }} />
+						<Navigate
+							to="/error"
+							state={{
+								previousPath,
+							}}
+						/>
 					)
 				) : (
 					checking && <Loading text={'Loading post ...'} />
 				)}
 				<div
-							className={`${styles.container} ${checking ? styles.hide : ''}`}
-							data-testid="container"
-						>
-							{post && (
-								<>
-									<Link to={-1} className={styles.link}>
-										<span
-											className={`${styles['left-arrow']} ${imageStyles.icon}`}
-										/>
-										Back to previous page
-									</Link>
+					className={`${styles.container} ${checking ? styles.hide : ''}`}
+					data-testid="container"
+				>
+					{post && (
+						<>
+							<Link to="/posts" className={styles.link}>
+								<span
+									className={`${styles['left-arrow']} ${imageStyles.icon}`}
+								/>
+								Back to list
+							</Link>
 
-									<h2 className={styles.title} title={post.title}>
-										{post.title}
-									</h2>
+							<h2 className={styles.title} title={post.title}>
+								{post.title}
+							</h2>
 
-									<div className={styles['published-date']}>
-										<strong>{post.author.username}</strong>
+							<div className={styles['published-date']}>
+								<strong>{post.author.username}</strong>
 
-										<em>{`Published in ${format(post.createdAt, 'MMMM d, y')}`}</em>
-										{new Date(post.createdAt).getDate() !==
-											new Date(post.updatedAt).getDate() && (
-											<em>{`Edited in ${format(post.updatedAt, 'MMMM d, y')}`}</em>
-										)}
-									</div>
+								<em>{`Published in ${format(post.createdAt, 'MMMM d, y')}`}</em>
+								{new Date(post.createdAt).getDate() !==
+									new Date(post.updatedAt).getDate() && (
+									<em>{`Edited in ${format(post.updatedAt, 'MMMM d, y')}`}</em>
+								)}
+							</div>
 
-									<div className={styles['image-wrap']}>
-										<div className={imageStyles.content} ref={imageContentRef}>
-											<img
-												title={post.title}
-												src={errorImage || post.mainImage}
-												alt="Main image"
-												onError={handleError}
-												onLoad={handleLoad}
-											/>
-										</div>
-									</div>
-								</>
-							)}
-							<Editor
-								id="editor-content"
-								initialValue={post?.content ?? ''}
-								onInit={(evt, editor) => {
-									setContentEditorLoad(false);
-									contentRef.current = editor;
-								}}
-								init={{
-									menubar: false,
-									toolbar: false,
-									inline: true,
-									plugins: 'codesample',
-								}}
-							/>
-						</div>
-					</div>
+							<div className={styles['image-wrap']}>
+								<div className={imageStyles.content} ref={imageContentRef}>
+									<img
+										title={post.title}
+										src={errorImage || post.mainImage}
+										alt="Main image"
+										onError={handleError}
+										onLoad={handleLoad}
+									/>
+								</div>
+							</div>
+						</>
+					)}
+					<Editor
+						id="editor-content"
+						initialValue={post?.content ?? ''}
+						onInit={(evt, editor) => {
+							setContentEditorLoad(false);
+							contentRef.current = editor;
+						}}
+						init={{
+							menubar: false,
+							toolbar: false,
+							inline: true,
+							plugins: 'codesample',
+						}}
+					/>
+				</div>
+			</div>
 			{!checking && (
 				<Comments
 					post={post}
