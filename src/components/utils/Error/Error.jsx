a1ad7bc1f8cd9d2286bb18717c9d7c1c988061fd
Weiss Bai <whitesgr03@gmail.com>
2025-05-13 13:00:21 +0800

    feat!: the error message is hard code now instead of rendering a value of the error property

--- src/components/utils/Error/Error.jsx
+++ src/components/utils/Error/Error.jsx
@@ -14,17 +14,11 @@ export const Error = ({ onReGetUser }) => {
 			<span className={`${imageStyles.icon} ${styles.alert}`} />
 			<div className={styles.message}>
 				<p>Our apologies, there has been an error.</p>
-				{state?.customMessage ? (
-					<p>{state?.error}</p>
-				) : (
-					<p>
-						Please come back later, or if you have any questions, contact us.
-					</p>
-				)}
+				<p>Please try again later, or if you have any questions, contact us.</p>
 			</div>
 			{state?.previousPath && (
 				<Link to={state.previousPath} className={styles.link}>
-					Go Back
+					Go Back Previous Page
 				</Link>
 			)}
 
