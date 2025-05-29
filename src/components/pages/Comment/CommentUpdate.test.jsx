42fe5bbd456d5dc43d269b6f60d54525ce60827d
Weiss Bai <whitesgr03@gmail.com>
2025-05-29 17:05:32 +0800

    test: added the react query module and mock the useAppDataAPI hook to all tests

--- src/components/pages/Comment/CommentUpdate.test.jsx
+++ src/components/pages/Comment/CommentUpdate.test.jsx
@@ -6,42 +6,40 @@ import {
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createMemoryRouter, Outlet } from 'react-router-dom';
 
 import { CommentUpdate } from './CommentUpdate';
 import { Loading } from '../../utils/Loading';
 
 import { updateComment } from '../../../utils/handleComment';
+import { useAppDataAPI } from '../App/AppContext';
 
-vi.mock('../../../components/utils/Loading');
-vi.mock('../../../utils/handlecomment');
+vi.mock('../../utils/Loading');
+vi.mock('../../../utils/handleComment');
+vi.mock('../App/AppContext');
 
 describe('CommentUpdate component', () => {
 	it('should close this component if the cancel button is clicked', async () => {
 		const user = userEvent.setup();
 		const mockProps = {
-			comment: {
-				content: 'comment',
-			},
+			content: 'comment',
 			onCloseCommentBox: vi.fn(),
+			commentId: '1',
 		};
 
-		const mockContext = {
+		const mockCustomHook = {
 			onAlert: vi.fn(),
-			onUpdatePost: vi.fn(),
+			onModal: vi.fn(),
 		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
 
+		const queryClient = new QueryClient();
 		const router = createMemoryRouter(
 			[
 				{
 					path: '/',
-					element: <Outlet context={{ ...mockContext }} />,
-					children: [
-						{
-							index: true,
-							element: <CommentUpdate {...mockProps} />,
-						},
-					],
+					element: <CommentUpdate {...mockProps} />,
 				},
 			],
 			{
@@ -52,12 +50,14 @@ describe('CommentUpdate component', () => {
 		);
 
 		render(
-			<RouterProvider
-				router={router}
-				future={{
-					v7_startTransition: true,
-				}}
-			/>,
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
 		);
 
 		const cancelButton = screen.getByRole('button', { name: 'Cancel' });
@@ -68,30 +68,28 @@ describe('CommentUpdate component', () => {
 	});
 	it('should change the comment field values if the comment field is entered', async () => {
 		const user = userEvent.setup();
-		const mockProps = {
-			comment: {
-				content: 'comment',
-			},
-		};
-
-		const mockContext = {
-			onAlert: vi.fn(),
-			onUpdatePost: vi.fn(),
-		};
 
 		const mockContent = '_changed';
 
+		const mockProps = {
+			content: 'comment',
+
+			onCloseCommentBox: vi.fn(),
+			commentId: '1',
+		};
+
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		const queryClient = new QueryClient();
 		const router = createMemoryRouter(
 			[
 				{
 					path: '/',
-					element: <Outlet context={{ ...mockContext }} />,
-					children: [
-						{
-							index: true,
-							element: <CommentUpdate {...mockProps} />,
-						},
-					],
+					element: <CommentUpdate {...mockProps} />,
 				},
 			],
 			{
@@ -102,46 +100,43 @@ describe('CommentUpdate component', () => {
 		);
 
 		render(
-			<RouterProvider
-				router={router}
-				future={{
-					v7_startTransition: true,
-				}}
-			/>,
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
 		);
 
-		const commentField = screen.getByDisplayValue(mockProps.comment.content);
+		const commentField = screen.getByDisplayValue(mockProps.content);
 
 		await user.type(commentField, mockContent);
 
-		expect(commentField).toHaveValue(
-			`${mockProps.comment.content}${mockContent}`,
-		);
+		expect(commentField).toHaveValue(`${mockProps.content}${mockContent}`);
 	});
 	it('should render an error field message if the field validation fails after submission', async () => {
 		const user = userEvent.setup();
 		const mockProps = {
-			comment: {
-				content: 'comment',
-			},
+			content: 'comment',
+
+			onCloseCommentBox: vi.fn(),
+			commentId: '1',
 		};
 
-		const mockContext = {
+		const mockCustomHook = {
 			onAlert: vi.fn(),
-			onUpdatePost: vi.fn(),
+			onModal: vi.fn(),
 		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
 
+		const queryClient = new QueryClient();
 		const router = createMemoryRouter(
 			[
 				{
 					path: '/',
-					element: <Outlet context={{ ...mockContext }} />,
-					children: [
-						{
-							index: true,
-							element: <CommentUpdate {...mockProps} />,
-						},
-					],
+					element: <CommentUpdate {...mockProps} />,
 				},
 			],
 			{
@@ -152,15 +147,17 @@ describe('CommentUpdate component', () => {
 		);
 
 		render(
-			<RouterProvider
-				router={router}
-				future={{
-					v7_startTransition: true,
-				}}
-			/>,
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
 		);
 
-		const submitButton = screen.getByRole('button', { name: 'Update' });
+		const submitButton = screen.getByRole('button', { name: 'Save' });
 
 		await user.click(submitButton);
 
@@ -172,30 +169,26 @@ describe('CommentUpdate component', () => {
 	});
 	it('should validate each input after a failed submission', async () => {
 		const user = userEvent.setup();
-		const mockProps = {
-			comment: {
-				content: 'comment',
-			},
-		};
-
-		const mockContext = {
-			onAlert: vi.fn(),
-			onUpdatePost: vi.fn(),
-		};
 
 		const mockContent = '_changed';
+		const mockProps = {
+			content: 'comment',
+			onCloseCommentBox: vi.fn(),
+			commentId: '1',
+		};
 
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		const queryClient = new QueryClient();
 		const router = createMemoryRouter(
 			[
 				{
 					path: '/',
-					element: <Outlet context={{ ...mockContext }} />,
-					children: [
-						{
-							index: true,
-							element: <CommentUpdate {...mockProps} />,
-						},
-					],
+					element: <CommentUpdate {...mockProps} />,
 				},
 			],
 			{
@@ -206,16 +199,18 @@ describe('CommentUpdate component', () => {
 		);
 
 		render(
-			<RouterProvider
-				router={router}
-				future={{
-					v7_startTransition: true,
-				}}
-			/>,
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
 		);
 
-		const submitButton = screen.getByRole('button', { name: 'Update' });
-		const commentField = screen.getByDisplayValue(mockProps.comment.content);
+		const submitButton = screen.getByRole('button', { name: 'Save' });
+		const commentField = screen.getByDisplayValue(mockProps.content);
 		const labelElement = screen.getByTestId('label');
 
 		await user.click(submitButton);
@@ -232,22 +227,6 @@ describe('CommentUpdate component', () => {
 	});
 	it('should render an error field message if the comment update fails', async () => {
 		const user = userEvent.setup();
-		const mockProps = {
-			post: {
-				_id: '0',
-				comments: [
-					{
-						_id: '0',
-						content: 'comment',
-					},
-				],
-			},
-		};
-
-		const mockContext = {
-			onAlert: vi.fn(),
-			onUpdatePost: vi.fn(),
-		};
 
 		const mockResolve = {
 			success: false,
@@ -258,25 +237,28 @@ describe('CommentUpdate component', () => {
 
 		const mockContent = '_changed';
 
-		updateComment.mockResolvedValueOnce(mockResolve);
-		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+		vi.mocked(updateComment).mockResolvedValueOnce(mockResolve);
+		vi.mocked(Loading).mockImplementationOnce(() => (
+			<div>Loading component</div>
+		));
+		const mockProps = {
+			content: 'comment',
+			onCloseCommentBox: vi.fn(),
+			commentId: '1',
+		};
 
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		const queryClient = new QueryClient();
 		const router = createMemoryRouter(
 			[
 				{
 					path: '/',
-					element: <Outlet context={{ ...mockContext }} />,
-					children: [
-						{
-							index: true,
-							element: (
-								<CommentUpdate
-									{...mockProps}
-									comment={mockProps.post.comments[0]}
-								/>
-							),
-						},
-					],
+					element: <CommentUpdate {...mockProps} />,
 				},
 			],
 			{
@@ -287,18 +269,18 @@ describe('CommentUpdate component', () => {
 		);
 
 		render(
-			<RouterProvider
-				router={router}
-				future={{
-					v7_startTransition: true,
-				}}
-			/>,
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
 		);
 
-		const submitButton = screen.getByRole('button', { name: 'Update' });
-		const commentField = screen.getByDisplayValue(
-			mockProps.post.comments[0].content,
-		);
+		const submitButton = screen.getByRole('button', { name: 'Save' });
+		const commentField = screen.getByDisplayValue(mockProps.content);
 		const labelElement = screen.getByTestId('label');
 
 		await user.type(commentField, mockContent);
@@ -317,48 +299,32 @@ describe('CommentUpdate component', () => {
 	});
 	it('should render an error message alert if the comment update fails', async () => {
 		const user = userEvent.setup();
-		const mockProps = {
-			post: {
-				_id: '0',
-				comments: [
-					{
-						_id: '0',
-						content: 'comment',
-					},
-				],
-			},
-		};
-
-		const mockContext = {
-			onAlert: vi.fn(),
-			onUpdatePost: vi.fn(),
-		};
-
-		const mockResolve = {
-			success: false,
-		};
 
 		const mockContent = '_changed';
+		vi.mocked(updateComment).mockImplementationOnce(
+			() => new Promise((_r, reject) => setTimeout(() => reject(Error()), 300)),
+		);
+		vi.mocked(Loading).mockImplementationOnce(() => (
+			<div>Loading component</div>
+		));
+		const mockProps = {
+			content: 'comment',
+			onCloseCommentBox: vi.fn(),
+			commentId: '1',
+		};
 
-		updateComment.mockResolvedValueOnce(mockResolve);
-		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
 
+		const queryClient = new QueryClient();
 		const router = createMemoryRouter(
 			[
 				{
 					path: '/',
-					element: <Outlet context={{ ...mockContext }} />,
-					children: [
-						{
-							index: true,
-							element: (
-								<CommentUpdate
-									{...mockProps}
-									comment={mockProps.post.comments[0]}
-								/>
-							),
-						},
-					],
+					element: <CommentUpdate {...mockProps} />,
 				},
 			],
 			{
@@ -369,77 +335,69 @@ describe('CommentUpdate component', () => {
 		);
 
 		render(
-			<RouterProvider
-				router={router}
-				future={{
-					v7_startTransition: true,
-				}}
-			/>,
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
 		);
 
-		const submitButton = screen.getByRole('button', { name: 'Update' });
-		const commentField = screen.getByDisplayValue(
-			mockProps.post.comments[0].content,
-		);
+		const submitButton = screen.getByRole('button', { name: 'Save' });
+		const commentField = screen.getByDisplayValue(mockProps.content);
 
 		await user.type(commentField, mockContent);
-		user.click(submitButton);
+		await user.click(submitButton);
 
-		const loadingComponent = await screen.findByText('Loading component');
+		await waitForElementToBeRemoved(() =>
+			screen.getByText('Loading component'),
+		);
 
 		expect(updateComment).toBeCalledTimes(1);
-		expect(mockContext.onAlert).toBeCalledTimes(1);
-		expect(loadingComponent).not.toBeInTheDocument();
+		expect(mockCustomHook.onAlert).toBeCalledTimes(1);
 	});
 	it('should update the comment if the comment field successfully validates after user submission', async () => {
 		const user = userEvent.setup();
-		const mockProps = {
-			post: {
-				_id: '0',
-				comments: [
-					{
-						_id: '0',
-						content: 'comment',
-					},
-				],
-			},
-			onCloseCommentBox: vi.fn(),
-			onUpdatePost: vi.fn(),
-		};
 
-		const mockContext = {
-			onAlert: vi.fn(),
-		};
-
+		const postId = '1';
 		const mockResolve = {
 			success: true,
 			data: {},
 		};
-
 		const mockContent = '_changed';
 
-		updateComment.mockResolvedValueOnce(mockResolve);
-		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+		vi.mocked(updateComment).mockResolvedValueOnce(mockResolve);
+		vi.mocked(Loading).mockImplementationOnce(() => (
+			<div>Loading component</div>
+		));
+		const mockProps = {
+			content: 'comment',
+			onCloseCommentBox: vi.fn(),
+			commentId: '1',
+		};
 
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		const queryClient = new QueryClient();
+		queryClient.setQueryData(['comments', postId], {
+			pages: [{ data: { comments: [{ _id: mockProps.commentId }] } }],
+			pageParams: {},
+		});
 		const router = createMemoryRouter(
 			[
 				{
-					path: '/',
-					element: <Outlet context={{ ...mockContext }} />,
-					children: [
-						{
-							index: true,
-							element: (
-								<CommentUpdate
-									{...mockProps}
-									comment={mockProps.post.comments[0]}
-								/>
-							),
-						},
-					],
+					path: `/post/:postId`,
+					element: <CommentUpdate {...mockProps} />,
 				},
 			],
 			{
+				initialEntries: [`/post/${postId}`],
 				future: {
 					v7_relativeSplatPath: true,
 				},
@@ -447,28 +405,24 @@ describe('CommentUpdate component', () => {
 		);
 
 		render(
-			<RouterProvider
-				router={router}
-				future={{
-					v7_startTransition: true,
-				}}
-			/>,
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
 		);
 
-		const submitButton = screen.getByRole('button', { name: 'Update' });
-		const commentField = screen.getByDisplayValue(
-			mockProps.post.comments[0].content,
-		);
+		const submitButton = screen.getByRole('button', { name: 'Save' });
+		const commentField = screen.getByDisplayValue(mockProps.content);
 
 		await user.type(commentField, mockContent);
-		user.click(submitButton);
-
-		const loadingComponent = await screen.findByText('Loading component');
+		await user.click(submitButton);
 
 		expect(updateComment).toBeCalledTimes(1);
-		expect(mockProps.onUpdatePost).toBeCalledTimes(1);
-		expect(mockContext.onAlert).toBeCalledTimes(1);
 		expect(mockProps.onCloseCommentBox).toBeCalledTimes(1);
-		expect(loadingComponent).not.toBeInTheDocument();
+		expect(mockCustomHook.onAlert).toBeCalledTimes(1);
 	});
 });
