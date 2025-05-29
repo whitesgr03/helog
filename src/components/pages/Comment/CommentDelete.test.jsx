21b867c950db19d8e362332c6ad671927778a01a
Weiss Bai <whitesgr03@gmail.com>
2025-05-29 17:32:25 +0800

    test: added the react query module and mock the useAppDataAPI hook to all tests

--- src/components/pages/Comment/CommentDelete.test.jsx
+++ src/components/pages/Comment/CommentDelete.test.jsx
@@ -1,97 +1,191 @@
 import { vi, describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import {
+	render,
+	screen,
+	waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 import { CommentDelete } from './CommentDelete';
 import { Loading } from '../../utils/Loading';
 
 import { deleteComment } from '../../../utils/handleComment';
+import { useAppDataAPI } from '../App/AppContext';
 
-vi.mock('../../../components/utils/Loading');
+vi.mock('../../utils/Loading');
 vi.mock('../../../utils/handleComment');
+vi.mock('../App/AppContext');
 
 describe('CommentDelete component', () => {
 	it('should render an error alert if delete a comment fails', async () => {
 		const user = userEvent.setup();
 		const mockProps = {
 			commentId: '0',
+		};
+		const postId = '1';
+
+		const mockCustomHook = {
 			onAlert: vi.fn(),
-			onActiveModal: vi.fn(),
+			onModal: vi.fn(),
 		};
-		const mockFetchResult = {
-			success: false,
-		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
 
-		deleteComment.mockResolvedValueOnce(mockFetchResult);
-		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+		vi.mocked(deleteComment).mockImplementationOnce(
+			() => new Promise((_r, reject) => setTimeout(() => reject(Error()), 300)),
+		);
+		vi.mocked(Loading).mockImplementationOnce(() => (
+			<div>Loading component</div>
+		));
 
-		render(<CommentDelete {...mockProps} />);
+		const queryClient = new QueryClient();
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: `/:postId`,
+					element: <CommentDelete {...mockProps} />,
+				},
+			],
+			{
+				initialEntries: [`/${postId}`],
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
+		);
 
 		const button = screen.getByRole('button', { name: 'Delete' });
 
-		user.click(button);
+		await user.click(button);
 
-		const loadingComponent = await screen.findByText('Loading component');
+		await waitForElementToBeRemoved(() =>
+			screen.getByText('Loading component'),
+		);
 
 		expect(deleteComment).toBeCalledTimes(1);
-		expect(mockProps.onAlert).toBeCalledTimes(1);
-		expect(mockProps.onActiveModal).toBeCalledTimes(1);
-		expect(loadingComponent).not.toBeInTheDocument();
-	});
-	it('should close modal if the cancel button is clicked', async () => {
-		const user = userEvent.setup();
-		const mockProps = {
-			onActiveModal: vi.fn(),
-		};
-
-		render(<CommentDelete {...mockProps} />);
-
-		const closeButton = screen.getByRole('button', { name: 'Cancel' });
-
-		await user.click(closeButton);
-
-		expect(mockProps.onActiveModal).toBeCalledTimes(1);
+		expect(mockCustomHook.onAlert).toBeCalledTimes(1);
+		expect(mockCustomHook.onModal).toBeCalledTimes(2);
 	});
 	it('should delete a comment if the delete button is clicked', async () => {
 		const user = userEvent.setup();
 		const mockProps = {
-			post: {
-				_id: '0',
-				comments: [
-					{
-						_id: '0',
-					},
-				],
-			},
-			onUpdatePost: vi.fn(),
-			onAlert: vi.fn(),
-			onActiveModal: vi.fn(),
+			commentId: '0',
 		};
 		const mockFetchResult = {
 			success: true,
 			data: {},
 		};
+		const postId = '1';
 
-		deleteComment.mockResolvedValueOnce(mockFetchResult);
-		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(deleteComment).mockResolvedValue(mockFetchResult);
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		const queryClient = new QueryClient();
+
+		queryClient.setQueryData(['comments', postId], {
+			pages: [{ data: { comments: [{ _id: mockProps.commentId }] } }],
+			pageParams: {},
+		});
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: `/:postId`,
+					element: <CommentDelete {...mockProps} />,
+				},
+			],
+			{
+				initialEntries: [`/${postId}`],
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
 
 		render(
-			<CommentDelete
-				{...mockProps}
-				commentId={mockProps.post.comments[0]._id}
-			/>,
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
 		);
 
 		const button = screen.getByRole('button', { name: 'Delete' });
 
-		user.click(button);
-
-		const loadingComponent = await screen.findByText('Loading component');
+		await user.click(button);
 
 		expect(deleteComment).toBeCalledTimes(1);
-		expect(mockProps.onUpdatePost).toBeCalledTimes(1);
-		expect(mockProps.onAlert).toBeCalledTimes(1);
-		expect(mockProps.onActiveModal).toBeCalledTimes(1);
-		expect(loadingComponent).not.toBeInTheDocument();
+		expect(deleteComment).toBeCalledTimes(1);
+		expect(mockCustomHook.onAlert).toBeCalledTimes(1);
+		expect(mockCustomHook.onModal).toBeCalledTimes(2);
+	});
+	it('should close modal if the cancel button is clicked', async () => {
+		const user = userEvent.setup();
+		const mockProps = {
+			commentId: '0',
+		};
+		const postId = '1';
+
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		const queryClient = new QueryClient();
+
+		const router = createMemoryRouter(
+			[
+				{
+					path: `/:postId`,
+					element: <CommentDelete {...mockProps} />,
+				},
+			],
+			{
+				initialEntries: [`/${postId}`],
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		render(
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider
+					router={router}
+					future={{
+						v7_startTransition: true,
+					}}
+				/>
+			</QueryClientProvider>,
+		);
+
+		const closeButton = screen.getByRole('button', { name: 'Cancel' });
+
+		await user.click(closeButton);
+
+		expect(mockCustomHook.onModal).toBeCalledTimes(1);
 	});
 });
