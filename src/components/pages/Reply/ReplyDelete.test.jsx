46bfa42e89f0440300ff21f60e48e3e29402878c
Weiss Bai <whitesgr03@gmail.com>
2025-05-29 19:22:03 +0800

    test: added the react query module and mock the useAppDataAPI hook to all tests

--- src/components/pages/Reply/ReplyDelete.test.jsx
+++ src/components/pages/Reply/ReplyDelete.test.jsx
@@ -1,100 +1,140 @@
 import { vi, describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import {
+	render,
+	screen,
+	waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ReplyDelete } from './ReplyDelete';
 import { Loading } from '../../utils/Loading';
 
 import { deleteReply } from '../../../utils/handleReply';
+import { useAppDataAPI } from '../App/AppContext';
 
-vi.mock('../../../components/utils/Loading');
+vi.mock('../../utils/Loading');
 vi.mock('../../../utils/handleReply');
+vi.mock('../App/AppContext');
 
 describe('ReplyDelete component', () => {
 	it('should render an error message alert if delete a reply fails', async () => {
 		const user = userEvent.setup();
 		const mockProps = {
-			replyId: 'test',
+			commentId: '1',
+			replyId: '1',
+		};
+
+		const mockCustomHook = {
 			onAlert: vi.fn(),
-			onActiveModal: vi.fn(),
+			onModal: vi.fn(),
 		};
-		const mockFetchResult = {
-			success: false,
-		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+		vi.mocked(deleteReply).mockImplementationOnce(
+			() => new Promise((_r, reject) => setTimeout(() => reject(Error()), 300)),
+		);
+		vi.mocked(Loading).mockImplementationOnce(() => (
+			<div>Loading component</div>
+		));
 
-		deleteReply.mockResolvedValueOnce(mockFetchResult);
-		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+		const queryClient = new QueryClient();
 
-		render(<ReplyDelete {...mockProps} />);
+		render(
+			<QueryClientProvider client={queryClient}>
+				<ReplyDelete {...mockProps} />
+			</QueryClientProvider>,
+		);
 
 		const button = screen.getByRole('button', { name: 'Delete' });
 
-		user.click(button);
+		await user.click(button);
 
-		const loadingComponent = await screen.findByText('Loading component');
+		await waitForElementToBeRemoved(() =>
+			screen.getByText('Loading component'),
+		);
 
 		expect(deleteReply).toBeCalledTimes(1);
-		expect(mockProps.onAlert).toBeCalledTimes(1);
-		expect(mockProps.onActiveModal).toBeCalledTimes(1);
-		expect(loadingComponent).not.toBeInTheDocument();
+		expect(mockCustomHook.onAlert).toBeCalledTimes(1);
+		expect(mockCustomHook.onModal).toBeCalledTimes(2);
 	});
 	it('should close modal if the cancel button is clicked', async () => {
 		const user = userEvent.setup();
 		const mockProps = {
-			onActiveModal: vi.fn(),
+			commentId: '1',
+			replyId: '1',
 		};
 
-		render(<ReplyDelete {...mockProps} />);
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+
+		const queryClient = new QueryClient();
+
+		render(
+			<QueryClientProvider client={queryClient}>
+				<ReplyDelete {...mockProps} />
+			</QueryClientProvider>,
+		);
 
 		const closeButton = screen.getByRole('button', { name: 'Cancel' });
 
 		await user.click(closeButton);
 
-		expect(mockProps.onActiveModal).toBeCalledTimes(1);
+		expect(mockCustomHook.onModal).toBeCalledTimes(1);
 	});
 	it('should delete a reply if the delete button is clicked', async () => {
 		const user = userEvent.setup();
-		const mockProps = {
-			post: {
-				_id: '0',
-				comments: [
-					{
-						_id: '0',
-						replies: [
-							{
-								_id: '0',
-								content: 'test reply',
-							},
-						],
-					},
-				],
-			},
-			commentId: '0',
-			replyId: '0',
-			onUpdatePost: vi.fn(),
-			onAlert: vi.fn(),
-			onActiveModal: vi.fn(),
-		};
+
 		const mockFetchResult = {
 			success: true,
 			data: {},
 		};
 
-		deleteReply.mockResolvedValueOnce(mockFetchResult);
-		Loading.mockImplementationOnce(() => <div>Loading component</div>);
+		const mockProps = {
+			commentId: '1',
+			replyId: '1',
+		};
 
-		render(<ReplyDelete {...mockProps} />);
+		const mockCustomHook = {
+			onAlert: vi.fn(),
+			onModal: vi.fn(),
+		};
+		vi.mocked(useAppDataAPI).mockReturnValue(mockCustomHook);
+		vi.mocked(deleteReply).mockImplementationOnce(
+			() =>
+				new Promise(resolve => setTimeout(() => resolve(mockFetchResult), 300)),
+		);
+		vi.mocked(Loading).mockImplementationOnce(() => (
+			<div>Loading component</div>
+		));
+
+		const queryClient = new QueryClient();
+
+		queryClient.setQueryData(['replies', mockProps.commentId], {
+			pages: [{ data: { replies: [{ _id: mockProps.replyId }] } }],
+			pageParams: {},
+		});
+
+		render(
+			<QueryClientProvider client={queryClient}>
+				<ReplyDelete {...mockProps} />
+			</QueryClientProvider>,
+		);
 
 		const button = screen.getByRole('button', { name: 'Delete' });
 
-		user.click(button);
+		await user.click(button);
 
-		const loadingComponent = await screen.findByText('Loading component');
+		await waitForElementToBeRemoved(() =>
+			screen.getByText('Loading component'),
+		);
 
 		expect(deleteReply).toBeCalledTimes(1);
-		expect(mockProps.onUpdatePost).toBeCalledTimes(1);
-		expect(mockProps.onAlert).toBeCalledTimes(1);
-		expect(mockProps.onActiveModal).toBeCalledTimes(1);
-		expect(loadingComponent).not.toBeInTheDocument();
+		expect(deleteReply).toBeCalledTimes(1);
+		expect(mockCustomHook.onAlert).toBeCalledTimes(1);
+		expect(mockCustomHook.onModal).toBeCalledTimes(2);
 	});
 });
