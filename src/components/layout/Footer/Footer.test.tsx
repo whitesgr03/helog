44da3eda98339115448d3711fc2f908e143fba26
Weiss Bai <whitesgr03@gmail.com>
2026-02-08 15:43:35 +0800

    test: update test and snapshot files

--- src/components/layout/Footer/Footer.test.tsx
+++ src/components/layout/Footer/Footer.test.tsx
@@ -1,11 +1,34 @@
 import { describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 import { Footer } from './Footer';
 
 describe('Footer component', () => {
 	it('should match snapshot', () => {
-		const { asFragment } = render(<Footer />);
+		const router = createMemoryRouter(
+			[
+				{
+					path: '/',
+					element: <Footer />,
+				},
+			],
+			{
+				initialEntries: ['/'],
+				future: {
+					v7_relativeSplatPath: true,
+				},
+			},
+		);
+
+		const { asFragment } = render(
+			<RouterProvider
+				router={router}
+				future={{
+					v7_startTransition: true,
+				}}
+			/>,
+		);
 
 		const actual = asFragment();
 
