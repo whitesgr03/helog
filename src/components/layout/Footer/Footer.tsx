93ef6f5212269a6e630a3a8395b8080f72d94f9e
Weiss Bai <whitesgr03@gmail.com>
2026-04-07 08:16:58 +0800

    feat: add the link text for improve accessibility

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -10,6 +10,7 @@ export const Footer = () => {
 			<div className={styles.container}>
 				<a href="https://github.com/whitesgr03/helog" className={styles.link}>
 					<span className={`${imageStyles.icon} ${styles.github}`} />
+					<em className={styles.emphasis}>Github repository</em>
 				</a>
 				<address>
 					<a href="mailto:whitesgr03@gmail.com" className={styles.link}>
