a53753ca58dd706d9a577b95ec86e813e320842e
Weiss Bai <whitesgr03@gmail.com>
2026-04-09 01:18:03 +0800

    feat!: remove the argument for the meta property that was passed to the useInfiniteQuery

--- src/components/pages/Home/LatestPosts.tsx
+++ src/components/pages/Home/LatestPosts.tsx
@@ -1,5 +1,4 @@
 // Packages
-import { useState } from 'react';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 // Styles
@@ -18,25 +17,10 @@ import { useAppDataAPI } from '../App/AppContext';
 
 export const LatestPosts = () => {
 	const { onAlert } = useAppDataAPI();
-	const [isManuallyRefetch, setIsManuallyRefetch] = useState(false);
 
-	const { isPending, isError, data, refetch } = useInfiniteQuery({
-		...infiniteQueryPostsOption(),
-		meta: {
-			errorAlert: () => {
-				isManuallyRefetch &&
-					onAlert([
-						{
-							message:
-								'Loading the posts has some errors occur, please try again later.',
-							error: true,
-							delay: 4000,
-						},
-					]);
-				setIsManuallyRefetch(false);
-			},
-		},
-	});
+	const { isPending, isError, data, refetch } = useInfiniteQuery(
+		infiniteQueryPostsOption(),
+	);
 
 	const posts = data?.pages[0].data.posts.slice(0, 4);
 
