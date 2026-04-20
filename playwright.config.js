6e70a1f15fd86db5361f7fd94ec31c566baaf912
Weiss Bai <whitesgr03@gmail.com>
2026-04-20 16:20:10 +0800

    chore: update playwright config file
    Load env variables.
    
    Add mobile test projects.

--- playwright.config.js
+++ playwright.config.js
@@ -2,22 +2,24 @@
 import { defineConfig, devices } from '@playwright/test';
 import 'dotenv/config';
 
+const PORT = process.env.PORT || 9001;
+
 export default defineConfig({
 	testDir: './src/E2E',
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	// forbidOnly: !!process.env.CI,
+	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
-	// retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
-	// workers: process.env.CI ? 1 : undefined,
+	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [['html', { open: 'never' }]],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: `http://localhost:${process.env.PORT}`,
+		baseURL: `http://localhost:${PORT}`,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
@@ -41,30 +43,20 @@ export default defineConfig({
 		},
 
 		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
-
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
+		{
+			name: 'Mobile Chrome',
+			use: { ...devices['Pixel 5'] },
+		},
+		{
+			name: 'Mobile Safari',
+			use: { ...devices['iPhone 12'] },
+		},
 	],
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: 'npm run dev',
-		url: `http://localhost:${process.env.PORT}`,
+		url: `http://localhost:${PORT}`,
 		reuseExistingServer: !process.env.CI,
 	},
 });
