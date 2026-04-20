325946046a00f244f5619bf5b69dc00744d42e39
Weiss Bai <whitesgr03@gmail.com>
2026-04-20 16:16:30 +0800

    chore: update config
    Load env variables in config.
    
    Update vite test properties

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,54 @@
 /// <reference types="vitest/config" />
+// add /// <reference types="vitest/config" /> to include the test types
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-	plugins: [react()],
-	preview: {
-		port: 9001,
-		headers: {
-			'X-Content-Type-Options': ' nosniff',
-			'Cross-Origin-Embedder-Policy': 'credentialless',
-			'X-Frame-Options': 'DENY',
-			'Cross-Origin-Resource-Policy': 'same-origin',
-			'Referrer-Policy': 'no-referrer',
-			'X-XSS-Protection': '0',
-			'X-DNS-Prefetch-Control': 'off',
-			'Strict-Transport-Security':
-				'max-age=63072000; includeSubDomains preload',
-			'Content-Security-Policy-Report-Only': `default-src 'none'; connect-src 'self' localhost:4000 *.tiny.cloud; script-src 'self' *.tiny.cloud 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com/css2 *.tiny.cloud; font-src https:; img-src 'self' https: data:; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests`,
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [react()],
+		preview: {
+			port: Number(env.PORT),
+			headers: {
+				'X-Content-Type-Options': 'nosniff',
+				'Cross-Origin-Embedder-Policy': 'credentialless',
+				'X-Frame-Options': 'DENY',
+				'Cross-Origin-Resource-Policy': 'same-origin',
+				'Referrer-Policy': 'no-referrer',
+				'X-XSS-Protection': '0',
+				'X-DNS-Prefetch-Control': 'off',
+				'Strict-Transport-Security':
+					'max-age=63072000; includeSubDomains preload',
+				'Content-Security-Policy-Report-Only': `default-src 'none'; connect-src 'self' ${env.VITE_RESOURCE_URL} *.tiny.cloud; script-src 'self' *.tiny.cloud 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com/css2 *.tiny.cloud; font-src https:; img-src 'self' https: data:; form-action 'self'; frame-ancestors 'none';`,
+			},
 		},
-	},
-	server: {
-		port: 9001,
-		headers: {
-			'X-Content-Type-Options': ' nosniff',
-			'Cross-Origin-Embedder-Policy': 'credentialless',
-			'X-Frame-Options': 'DENY',
-			'Cross-Origin-Resource-Policy': 'same-origin',
-			'Referrer-Policy': 'no-referrer',
-			'X-XSS-Protection': '0',
-			'X-DNS-Prefetch-Control': 'off',
-			'Strict-Transport-Security':
-				'max-age=63072000; includeSubDomains preload',
-			'Content-Security-Policy-Report-Only': `default-src 'none'; connect-src 'self' localhost:4000 *.tiny.cloud; script-src 'self' *.tiny.cloud 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com/css2 *.tiny.cloud; font-src https:; img-src 'self' https: data:; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests`,
+		server: {
+			port: Number(env.PORT),
+			headers: {
+				'X-Content-Type-Options': 'nosniff',
+				'Cross-Origin-Embedder-Policy': 'credentialless',
+				'X-Frame-Options': 'DENY',
+				'Cross-Origin-Resource-Policy': 'same-origin',
+				'Referrer-Policy': 'no-referrer',
+				'X-XSS-Protection': '0',
+				'X-DNS-Prefetch-Control': 'off',
+				'Strict-Transport-Security':
+					'max-age=63072000; includeSubDomains preload',
+				'Content-Security-Policy-Report-Only': `default-src 'none'; connect-src 'self' ${env.VITE_RESOURCE_URL} *.tiny.cloud; script-src 'self' *.tiny.cloud 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com/css2 *.tiny.cloud; font-src https:; img-src 'self' https: data:; form-action 'self'; frame-ancestors 'none';`,
+			},
 		},
-	},
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './src/setup.ts',
-		include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-		exclude: ['src/**/delete_*.{test,spec}.?(c|m)[jt]s?(x)', 'src/E2E'],
-		coverage: {
-			include: ['src/**'],
-			exclude: ['src/**/delete_**'],
+		test: {
+			globals: true,
+			environment: 'jsdom',
+			setupFiles: './src/setup.ts',
+			include: ['src/**/*.test.ts?(x)'],
+			exclude: ['src/**/delete_*', 'src/E2E'],
+			coverage: {
+				include: ['src/**/*.ts?(x)'],
+				exclude: ['src/**/delete_*'],
+			},
 		},
-	},
+	};
 });
