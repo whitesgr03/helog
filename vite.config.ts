39da2ff77849c91d0d02291b43189b009152c0a2
Weiss Bai <whitesgr03@gmail.com>
2026-05-14 13:32:41 +0800

    chore: update comment for better clearly

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest/config" />
-// add /// <reference types="vitest/config" /> to include the test types
+// Reference the test property
 
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
